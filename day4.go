07d4ee1924d52d0a8efafd2d4a4a06844d34eab0
Dave King <tildedave@gmail.com>
2023-12-04 06:36:50 -0500

    Day 4: second star

--- day4.go
+++ day4.go
@@ -9,6 +9,11 @@ import (
 	"strings"
 )
 
+type gameResult struct {
+	winningNumbers map[int]bool
+	gameNumbers    map[int]bool
+}
+
 func parseNumberList(str string) map[int]bool {
 	var nums map[int]bool = make(map[int]bool)
 	for _, numStr := range strings.Fields(str) {
@@ -23,23 +28,42 @@ func parseNumberList(str string) map[int]bool {
 
 func day4(f *os.File) {
 	scanner := bufio.NewScanner(f)
-	sum := 0
+	cardNum := 0
+	cardData := make(map[int]gameResult)
+	cardCount := make(map[int]int)
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		res := strings.Split(strings.Split(line, ": ")[1], " | ")
+		cardNum += 1
+		splitLine := strings.Split(line, ": ")
+		res := strings.Split(splitLine[1], " | ")
 		winningNumbers := parseNumberList(res[0])
 		myNumbers := parseNumberList(res[1])
-		var score = 0
-		for num := range myNumbers {
-			if winningNumbers[num] {
-				if score == 0 {
-					score = 1
-				} else {
-					score *= 2
+		result := gameResult{winningNumbers: winningNumbers, gameNumbers: myNumbers}
+		cardData[cardNum] = result
+	}
+	maxCard := cardNum
+
+	for cardNum = 1; cardNum <= maxCard; cardNum++ {
+		result := cardData[cardNum]
+		cardCount[cardNum] += 1
+		for j := 0; j < cardCount[cardNum]; j++ {
+			var score = 0
+			for num := range result.gameNumbers {
+				if result.winningNumbers[num] {
+					score += 1
+				}
+			}
+			for i := cardNum + 1; i < cardNum+1+score; i++ {
+				if len(cardData[i].gameNumbers) > 0 { // stupid nullity check
+					cardCount[i] += 1
 				}
 			}
 		}
-		sum += score
+	}
+	sum := 0
+	for _, v := range cardCount {
+		sum += v
 	}
 	fmt.Println(sum)
 }
