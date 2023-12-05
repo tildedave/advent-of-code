f27c1de3a96bd644f945126d29a227b9d195dd0e
Dave King <tildedave@gmail.com>
2023-12-05 12:53:06 -0500

    Day 5: second star <sweat>

--- day5.go
+++ day5.go
@@ -13,11 +13,11 @@ import (
 func extractFields(s string) []int {
 	var nums []int
 	for _, str := range strings.Fields(s) {
-		seedNum, err := strconv.ParseInt(str, 10, 64)
+		n, err := strconv.ParseInt(str, 10, 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		nums = append(nums, int(seedNum))
+		nums = append(nums, int(n))
 	}
 	return nums
 }
@@ -28,9 +28,55 @@ type rangeMapping = struct {
 	length      int
 }
 
+type entityRange = struct {
+	start  int
+	length int
+}
+
+func totalLength(rs []entityRange) int {
+	// For sanity checking
+	sum := 0
+	for _, r := range rs {
+		sum += r.length
+	}
+	return sum
+}
+
+func alignRanges(r rangeMapping, currentRange entityRange) ([]entityRange, []entityRange, bool) {
+	mappingEnd := r.sourceStart + r.length
+	currentEnd := currentRange.start + currentRange.length
+
+	if currentRange.start >= r.sourceStart && currentEnd <= mappingEnd {
+		// Option 1: currentRange contained in r.
+		return []entityRange{currentRange}, []entityRange{}, true
+	} else if currentRange.start < r.sourceStart && currentEnd <= mappingEnd && currentEnd > r.sourceStart {
+		// Option 2: Split into 2 on the left
+		leftLength := r.sourceStart - currentRange.start
+		leftRange := entityRange{start: currentRange.start, length: leftLength}
+		mappingRange := entityRange{start: r.sourceStart, length: currentRange.length - leftLength}
+
+		return []entityRange{mappingRange}, []entityRange{leftRange}, true
+	} else if currentRange.start > r.sourceStart && currentRange.start < mappingEnd && mappingEnd > currentEnd {
+		// Option 3: Split into 2 on the right
+		mappingLength := mappingEnd - currentRange.start
+		mappingRange := entityRange{start: currentRange.start, length: mappingLength}
+		rightRange := entityRange{start: r.sourceStart + r.length, length: currentRange.length - mappingLength}
+
+		return []entityRange{mappingRange}, []entityRange{rightRange}, true
+	} else if currentRange.start < r.sourceStart && currentEnd > mappingEnd {
+		// Option 4: Split into 3
+		leftRange := entityRange{start: currentRange.start, length: r.sourceStart - currentRange.start}
+		mappingRange := entityRange{start: r.sourceStart, length: r.length}
+		rightRange := entityRange{start: r.sourceStart + r.length, length: currentRange.length - mappingRange.length - leftRange.length}
+
+		return []entityRange{mappingRange}, []entityRange{leftRange, rightRange}, true
+	}
+
+	return []entityRange{}, []entityRange{}, false
+}
+
 func day5(f *os.File) {
 	scanner := bufio.NewScanner(f)
-	var seedNums []int
 	var currentMapSource string
 	var currentMapDest string
 
@@ -39,11 +85,21 @@ func day5(f *os.File) {
 		sourceStart int
 		length      int
 	})
+	var entityRanges map[string][]entityRange = make(map[string][]struct {
+		start  int
+		length int
+	})
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		if strings.HasPrefix(line, "seeds: ") {
-			seedNums = extractFields(strings.Split(line, "seeds: ")[1])
+			seedNums := extractFields(strings.Split(line, "seeds: ")[1])
+			var seedRanges []entityRange
+			for i := 0; i < len(seedNums); i += 2 {
+				r := entityRange{start: seedNums[i], length: seedNums[i+1]}
+				seedRanges = append(seedRanges, r)
+			}
+			entityRanges["seed"] = seedRanges
 			continue
 		}
 
@@ -78,43 +134,96 @@ func day5(f *os.File) {
 	}
 
 	smallestLocationSeen := math.MaxInt
-	for _, seedNum := range seedNums {
+	for _, r := range entityRanges["seed"] {
 		// So now we go through this ridiculous mapping process
 		// seems like it's just a straight shot through the map at least
 		// actually seems like the map is just a straight shot.
 		currentEntity := "seed"
-		currentNum := seedNum
+		// So we have a range, and we want to turn it into *more* ranges.
+		// The puzzle input ranges are too big to realistically map number by number.
+		currentEntityRanges := []entityRange{r}
 		for {
 			var nextEntity string
-			var nextNum int = -1
+			// var nextEntityRanges []entityRange
 			for k := range mapRanges[currentEntity] {
 				nextEntity = k
 				break
 			}
-			// Now we map the current num to the next num.
-			// We go through the listing for the current entity to see if any
-			// match.
-			rangeMappings := mapRanges[currentEntity][nextEntity]
-			for _, rangeMapping := range rangeMappings {
-				if currentNum >= rangeMapping.sourceStart && currentNum < rangeMapping.sourceStart+rangeMapping.length {
-					offset := currentNum - rangeMapping.sourceStart
-					nextNum = rangeMapping.destStart + offset
-					break
+
+			// alignment portion
+			var alignedRanges []entityRange
+			iterRanges := make([]entityRange, len(currentEntityRanges))
+			copy(iterRanges, currentEntityRanges)
+
+			for len(iterRanges) > 0 {
+				rangeMappings := mapRanges[currentEntity][nextEntity]
+				currentRange := iterRanges[0]
+				iterRanges = iterRanges[1:]
+
+				intersected := false
+				for _, rangeMapping := range rangeMappings {
+					ars, unalignedRanges, done := alignRanges(rangeMapping, currentRange)
+					if !(len(ars) == 0 && len(unalignedRanges) == 0) {
+						intersected = true
+					}
+					for _, ar := range ars {
+						alignedRanges = append(alignedRanges, ar)
+					}
+					for _, r := range unalignedRanges {
+						iterRanges = append(iterRanges, r)
+					}
+					if done {
+						break
+					}
+				}
+				// Totally disjoint
+				if !intersected {
+					alignedRanges = append(alignedRanges, currentRange)
 				}
 			}
-			if nextNum == -1 {
-				nextNum = currentNum
+
+			if totalLength(currentEntityRanges) != totalLength(alignedRanges) {
+				panic("Split lengths did not match")
+			}
+
+			// next entity portion
+			var nextRanges []entityRange
+			for _, currentRange := range alignedRanges {
+				rangeMappings := mapRanges[currentEntity][nextEntity]
+				currentEnd := currentRange.start + currentRange.length
+
+				wasMapped := false
+				for _, rangeMapping := range rangeMappings {
+					// either it is totally disjoint or totally contained
+					mappingStart := rangeMapping.sourceStart
+					mappingEnd := mappingStart + rangeMapping.length
+					if currentRange.start >= mappingStart && currentEnd <= mappingEnd {
+						// do the mapping
+						offset := currentRange.start - rangeMapping.sourceStart
+						mappedRange := entityRange{start: rangeMapping.destStart + offset, length: currentRange.length}
+						nextRanges = append(nextRanges, mappedRange)
+						wasMapped = true
+						break
+					}
+				}
+				if !wasMapped {
+					nextRanges = append(nextRanges, currentRange)
+				}
 			}
 
 			if currentEntity == "location" && nextEntity == "" {
-				if nextNum < smallestLocationSeen {
-					smallestLocationSeen = nextNum
+				// What's our smallest number?
+				// It will be the smallest start of the remaining
+				for _, r := range nextRanges {
+					if r.start < smallestLocationSeen {
+						smallestLocationSeen = r.start
+					}
 				}
 				break
 			}
-			// fmt.Printf("%s %d corresponds to %s %d\n", currentEntity, currentNum, nextEntity, nextNum)
+
 			currentEntity = nextEntity
-			currentNum = nextNum
+			currentEntityRanges = nextRanges
 		}
 	}
 	fmt.Println(smallestLocationSeen)
