d419e773b2662ee5b88ea9544d2101f767cb51be
Dave King <tildedave@gmail.com>
2023-12-06 07:20:17 -0500

    Day 6: second star

--- day6.go
+++ day6.go
@@ -9,30 +9,25 @@ import (
 
 func day6(f *os.File) {
 	scanner := bufio.NewScanner(f)
-	var times []int
-	var distances []int
+	var time int
+	var distance int
 	for scanner.Scan() {
 		text := scanner.Text()
 		if strings.HasPrefix(text, "Time:") {
-			times = extractFields(strings.TrimSpace(strings.Split(text, "Time:")[1]))
+			time = extractFields(strings.ReplaceAll(strings.TrimSpace(strings.Split(text, "Time:")[1]), " ", ""))[0]
 		}
 		if strings.HasPrefix(text, "Distance:") {
-			distances = extractFields(strings.TrimSpace(strings.Split(text, "Distance:")[1]))
+			distance = extractFields(strings.ReplaceAll(strings.TrimSpace(strings.Split(text, "Distance:")[1]), " ", ""))[0]
 		}
 	}
-	totalTimes := 1
-	for i := 0; i < len(times); i++ {
-		time, distance := times[i], distances[i]
-		waysToWin := 0
-		for j := 0; j < time; j++ {
-			// simulate hold down for j seconds
-			// hold down for j seconds means we travel j * (time - j) distance
-			traveledDistance := j * (time - j)
-			if traveledDistance > distance {
-				waysToWin++
-			}
+	waysToWin := 0
+	for j := 0; j < time; j++ {
+		// simulate hold down for j seconds
+		// hold down for j seconds means we travel j * (time - j) distance
+		traveledDistance := j * (time - j)
+		if traveledDistance > distance {
+			waysToWin++
 		}
-		totalTimes *= waysToWin
 	}
-	fmt.Println(totalTimes)
+	fmt.Println(waysToWin)
 }
