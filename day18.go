0fc60c464ae1e5f0b5af4fdbf09eba1ba567bf22
Dave King <tildedave@gmail.com>
2023-12-18 19:43:24 -0500

    Day 18: use shoelace theorem

--- day18.go
+++ day18.go
@@ -53,154 +53,38 @@ func day18(f *os.File) {
 		instructions = append(instructions, e)
 	}
 
-	// currentX := 0
-	// currentY := 0
-	// maxX := 0
-	// maxY := 0
-	// minX := 0
-	// minY := 0
-	// for _, e := range instructions {
-	// 	switch e.direction {
-	// 	case DIR_LEFT:
-	// 		currentY -= e.length
-	// 	case DIR_RIGHT:
-	// 		currentY += e.length
-	// 	case DIR_UP:
-	// 		currentX -= e.length
-	// 	case DIR_DOWN:
-	// 		currentX += e.length
-	// 	}
-	// 	if currentX > maxX {
-	// 		maxX = currentX
-	// 	}
-	// 	if currentY > maxY {
-	// 		maxY = currentY
-	// 	}
-	// 	if currentX < minX {
-	// 		minX = currentX
-	// 	}
-	// 	if currentY < minY {
-	// 		minY = currentY
-	// 	}
-	// }
-	// maxX += 1
-	// maxY += 1
-	// // Add a moat around the whole thing so that
-	// // x = 0, y = 0, x = len(grid) - 1, y = len(grid[0]) - 1 are all outside.
-	// maxX += 2
-	// maxY += 2
-	// minX -= 2
-	// minY -= 2
-
-	// Maybe I should have used a map instead of this grid thing.
-	// fmt.Println(minX, minY, maxX, maxY, maxX-minX)
-	// grid := make([][]edge, maxX-minX)
-	// for i := 0; i < maxX-minX; i++ {
-	// 	grid[i] = make([]edge, maxY-minY)
-	// }
-
-	grid := make(map[string]edge)
 	currentX := 0
 	currentY := 0
-	maxX := 0
-	maxY := 0
-	minX := 0
-	minY := 0
+	vertices := make([][]int, 0)
+	perimLength := 0
 
 	for _, e := range instructions {
+		// fmt.Println(currentX, currentY)
+		vertices = append(vertices, []int{currentX, currentY})
 		switch e.direction {
 		case DIR_LEFT:
-			for i := 0; i < e.length; i++ {
-				grid[gridKey(currentX, currentY-i)] = e
-			}
 			currentY -= e.length
 		case DIR_RIGHT:
-			for i := 0; i < e.length; i++ {
-				grid[gridKey(currentX, currentY+i)] = e
-			}
 			currentY += e.length
 		case DIR_UP:
-			for i := 0; i < e.length; i++ {
-				grid[gridKey(currentX-i, currentY)] = e
-			}
 			currentX -= e.length
 		case DIR_DOWN:
-			for i := 0; i < e.length; i++ {
-				grid[gridKey(currentX+i, currentY)] = e
-			}
 			currentX += e.length
 		}
-		if currentX > maxX {
-			maxX = currentX
-		}
-		if currentY > maxY {
-			maxY = currentY
-		}
-		if currentX < minX {
-			minX = currentX
-		}
-		if currentY < minY {
-			minY = currentY
-		}
+		perimLength += e.length
 	}
-
-	// flood fill needs to start from the outside, so we add a moat of 1 line
-	// around everything
-	visited := make(map[string]bool)
-	queue := make([][]int, 0)
-	for x := minX - 1; x <= maxX+1; x++ {
-		queue = append(queue, []int{x, minY - 1})
-		queue = append(queue, []int{x, maxY + 1})
+	if currentX != 0 || currentY != 0 {
+		fmt.Println(currentX, currentY)
+		panic("Did not return to start")
 	}
-	for y := minY - 1; y <= maxY+1; y++ {
-		queue = append(queue, []int{minX - 1, y})
-		queue = append(queue, []int{maxX + 1, y})
+	// Shoelace formula, 2A = |x1 x2| + |x2 x3| + ...
+	//                        |y1 y2| + |y2 y3|
+	sum := 0
+	for i, curr := range vertices {
+		nextIndex := (i + 1) % len(vertices)
+		next := vertices[nextIndex]
+		sum += (curr[1] + next[1]) * (curr[0] - next[0])
 	}
-	for len(queue) > 0 {
-		r := queue[0]
-		queue = queue[1:]
-		x, y := r[0], r[1]
-		k := gridKey(x, y)
-		if grid[k].length != 0 {
-			panic("Hit an edge, should not do this")
-		}
-
-		if visited[k] {
-			continue
-		}
-		visited[k] = true
-		canUp := x > minX && grid[gridKey(x-1, y)].length == 0
-		canDown := x < maxX && grid[gridKey(x+1, y)].length == 0
-		canLeft := y > minY && grid[gridKey(x, y-1)].length == 0
-		canRight := y < maxY && grid[gridKey(x, y+1)].length == 0
-		if canUp {
-			queue = append(queue, []int{x - 1, y})
-		}
-		if canDown {
-			queue = append(queue, []int{x + 1, y})
-		}
-		if canLeft {
-			queue = append(queue, []int{x, y - 1})
-		}
-		if canRight {
-			queue = append(queue, []int{x, y + 1})
-		}
-	}
-
-	count := 0
-	str := ""
-	for i := minX - 1; i <= maxX+1; i++ {
-		line := ""
-		for j := minY - 1; j <= maxY+1; j++ {
-			if visited[gridKey(i, j)] {
-				line += "#"
-			} else {
-				line += "."
-				count++
-			}
-		}
-		str += line + "\n"
-	}
-	fmt.Println(str)
-	fmt.Println(count)
+	// Why is this right?
+	fmt.Println(-sum/2 + perimLength/2 + 1)
 }
