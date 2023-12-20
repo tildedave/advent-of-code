85c842941d140782b926576b6b9d8cfd5c6ac601
Dave King <tildedave@gmail.com>
2023-12-20 15:22:16 -0500

    Day 20: some clean-up

--- day20.go
+++ day20.go
@@ -21,18 +21,15 @@ type queueItem = struct {
 	pulse  bool
 }
 
-func summarizeStates(
-	states []string,
-	flipFlopStates map[string]bool,
-	conjunctionStates map[string]map[string]bool,
-	connections map[string][]string,
-	types map[string]int) (string, int64) {
+func summarizeState(
+	state *MachineState,
+) (string, int64) {
 	ffRet := ""
 	ffNum := int64(0)
 	conjRet := ""
-	for _, k := range states {
-		if types[k] == TYPE_FLIP_FLOP {
-			if flipFlopStates[k] {
+	for _, k := range state.labels {
+		if state.types[k] == TYPE_FLIP_FLOP {
+			if state.flipFlopStates[k] {
 				ffRet = ffRet + "1"
 				ffNum = (ffNum << 1) | 1
 			} else {
@@ -40,10 +37,10 @@ func summarizeStates(
 				ffNum = (ffNum << 1) | 0
 			}
 		}
-		if types[k] == TYPE_CONJUNCTION {
+		if state.types[k] == TYPE_CONJUNCTION {
 			conjRet += k + "{"
-			for _, s := range connections[k] {
-				if conjunctionStates[k][s] {
+			for _, s := range state.connections[k] {
+				if state.conjunctionStates[k][s] {
 					conjRet += "1"
 				} else {
 					conjRet += "0"
@@ -65,14 +62,97 @@ func (s slice) Less(d, e int) bool {
 	return t < u
 }
 
+func toDOTString(
+	state *MachineState,
+) string {
+	ret := ""
+	ret += "digraph {"
+	for _, k := range state.labels {
+		if state.types[k] == TYPE_FLIP_FLOP {
+			ret += fmt.Sprintf("%s [label=\"%%%s\"]\n", k, k)
+		} else if state.types[k] == TYPE_CONJUNCTION {
+			ret += fmt.Sprintf("%s [label=\"&%s\"]\n", k, k)
+		}
+		for _, c := range state.connections[k] {
+			ret += fmt.Sprintf("%s -> %s;\n", k, c)
+		}
+	}
+	ret += fmt.Sprintf("}")
+	return ret
+}
+
+type MachineState = struct {
+	labels            []string
+	connections       map[string][]string
+	types             map[string]int
+	flipFlopStates    map[string]bool
+	conjunctionStates map[string]map[string]bool
+}
+
+func pushButton(
+	state *MachineState,
+) (int, int, bool) {
+	numHigh := 0
+	numLow := 0
+
+	queue := make([]queueItem, 0)
+	queue = append(queue, queueItem{"button", "broadcaster", false})
+	for len(queue) > 0 {
+		item := queue[0]
+		// var pulseLabel string
+		// if item.pulse {
+		// 	pulseLabel = "high"
+		// } else {
+		// 	pulseLabel = "low"
+		// }
+		// fmt.Printf("%s -%s-> %s\n", item.source, pulseLabel, item.label)
+		queue = queue[1:]
+		if item.pulse {
+			numHigh++
+		} else {
+			numLow++
+		}
+		if item.label == "rx" && !item.pulse {
+			return numHigh, numLow, true
+		}
+		switch state.types[item.label] {
+		case TYPE_BROADCASTER:
+			for _, c := range state.connections[item.label] {
+				queue = append(queue, queueItem{item.label, c, item.pulse})
+			}
+		case TYPE_FLIP_FLOP:
+			if !item.pulse {
+				newState := !state.flipFlopStates[item.label]
+				state.flipFlopStates[item.label] = newState
+				for _, c := range state.connections[item.label] {
+					queue = append(queue, queueItem{item.label, c, newState})
+				}
+			}
+		case TYPE_CONJUNCTION:
+			state.conjunctionStates[item.label][item.source] = item.pulse
+			// sends low if all inputs have memory of high, sends high
+			// otherwise.
+			newState := true
+			for _, v := range state.conjunctionStates[item.label] {
+				newState = newState && v
+			}
+
+			for _, c := range state.connections[item.label] {
+				queue = append(queue, queueItem{item.label, c, !newState})
+			}
+		}
+	}
+	return numHigh, numLow, false
+}
+
 func day20(f *os.File) {
 	scanner := bufio.NewScanner(f)
-	connections := make(map[string][]string)
-	types := make(map[string]int)
-
-	flipFlopStates := make(map[string]bool)
-	conjunctionStates := make(map[string]map[string]bool)
-	states := make([]string, 0)
+	state := MachineState{}
+	state.connections = make(map[string][]string)
+	state.types = make(map[string]int)
+	state.flipFlopStates = make(map[string]bool)
+	state.conjunctionStates = make(map[string]map[string]bool)
+	state.labels = make([]string, 0)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -85,7 +165,7 @@ func day20(f *os.File) {
 		} else if lhs[0] == '&' {
 			lhs = lhs[1:]
 			lhsType = TYPE_CONJUNCTION
-			conjunctionStates[lhs] = make(map[string]bool)
+			state.conjunctionStates[lhs] = make(map[string]bool)
 		} else if lhs == "broadcaster" {
 			lhsType = TYPE_BROADCASTER
 		} else {
@@ -97,28 +177,28 @@ func day20(f *os.File) {
 			connects = append(connects, strings.TrimSpace(rhs))
 		}
 
-		connections[lhs] = connects
-		types[lhs] = lhsType
-		states = append(states, lhs)
+		state.connections[lhs] = connects
+		state.types[lhs] = lhsType
+		state.labels = append(state.labels, lhs)
 	}
 
-	a := slice{states}
+	a := slice{state.labels}
 	sort.Sort(a)
-	states = a.StringSlice
+	state.labels = a.StringSlice
 
 	// set up the conjunction states
-	for k, v := range connections {
+	for k, v := range state.connections {
 		for _, b := range v {
-			if types[b] == TYPE_CONJUNCTION {
-				conjunctionStates[b][k] = false
+			if state.types[b] == TYPE_CONJUNCTION {
+				state.conjunctionStates[b][k] = false
 			}
 		}
 	}
 
 	top := ""
 	bottom := ""
-	for _, k := range states {
-		if types[k] == TYPE_FLIP_FLOP {
+	for _, k := range state.labels {
+		if state.types[k] == TYPE_FLIP_FLOP {
 			top += string(k[0])
 			bottom += string(k[1])
 		}
@@ -126,76 +206,18 @@ func day20(f *os.File) {
 	fmt.Println(top)
 	fmt.Println(bottom)
 
-	// fmt.Println("digraph {")
-	// for _, k := range states {
-	// 	if types[k] == TYPE_FLIP_FLOP {
-	// 		fmt.Printf("%s [label=\"%%%s\"]\n", k, k)
-	// 	} else if types[k] == TYPE_CONJUNCTION {
-	// 		fmt.Printf("%s [label=\"&%s\"]\n", k, k)
-	// 	}
-	// 	for _, c := range connections[k] {
-	// 		fmt.Printf("%s -> %s;\n", k, c)
-	// 	}
-	// }
-	// fmt.Println("}")
-
 	numHigh := 0
 	numLow := 0
 	numPushes := 0
 	prevN := int64(-1)
-	for numPushes < 1024 {
-		s, n := summarizeStates(states, flipFlopStates, conjunctionStates, connections, types)
+	for numPushes < 1000 {
+		s, n := summarizeState(&state)
 		fmt.Println(s, n^prevN)
 		prevN = n
 		numPushes++
-		queue := make([]queueItem, 0)
-		queue = append(queue, queueItem{"button", "broadcaster", false})
-		for len(queue) > 0 {
-			item := queue[0]
-			// var pulseLabel string
-			// if item.pulse {
-			// 	pulseLabel = "high"
-			// } else {
-			// 	pulseLabel = "low"
-			// }
-			// fmt.Printf("%s -%s-> %s\n", item.source, pulseLabel, item.label)
-			queue = queue[1:]
-			if item.pulse {
-				numHigh++
-			} else {
-				numLow++
-			}
-			if item.label == "rx" && !item.pulse {
-				fmt.Println(numPushes)
-				return
-			}
-			switch types[item.label] {
-			case TYPE_BROADCASTER:
-				for _, c := range connections[item.label] {
-					queue = append(queue, queueItem{item.label, c, item.pulse})
-				}
-			case TYPE_FLIP_FLOP:
-				if !item.pulse {
-					newState := !flipFlopStates[item.label]
-					flipFlopStates[item.label] = newState
-					for _, c := range connections[item.label] {
-						queue = append(queue, queueItem{item.label, c, newState})
-					}
-				}
-			case TYPE_CONJUNCTION:
-				conjunctionStates[item.label][item.source] = item.pulse
-				// sends low if all inputs have memory of high, sends high
-				// otherwise.
-				newState := true
-				for _, v := range conjunctionStates[item.label] {
-					newState = newState && v
-				}
-
-				for _, c := range connections[item.label] {
-					queue = append(queue, queueItem{item.label, c, !newState})
-				}
-			}
-		}
+		high, low, _ := pushButton(&state)
+		numHigh += high
+		numLow += low
 	}
 	fmt.Println(numHigh * numLow)
 }
