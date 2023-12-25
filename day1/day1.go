a8d764dccad0cd93cdb4570c4d55c758f590a78d
Dave King <tildedave@gmail.com>
2023-12-25 12:38:56 -0500

    Day 1: second star

--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,36 @@
 package day1
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func TotalFuel(n int) int {
+	total := 0
+	for {
+		fuelAmount := n/3 - 2
+		if fuelAmount <= 0 {
+			break
+		}
+		total += fuelAmount
+		n = fuelAmount
+	}
+	return total
+}
 
 func Run(f *os.File) {
-
+	scanner := bufio.NewScanner(f)
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		n, err := strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		total += TotalFuel(int(n))
+	}
+	fmt.Println(total)
 }
