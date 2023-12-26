67b6e4686ff4ff9f7de5c4508d3d4b28b12c7d0b
Dave King <tildedave@gmail.com>
2023-12-26 09:42:59 -0500

    Intocde: get position/immediate working (basic)

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -1,6 +1,7 @@
 package intcode
 
 import (
+	"errors"
 	"log"
 )
 
@@ -19,7 +20,21 @@ var arity = map[int]int{
 	ENDING_OPCODE: 0,
 }
 
-func Exec(program []int) []int {
+var args = map[int]int{
+	ADD_OPCODE:    2,
+	MULT_OPCODE:   2,
+	SAVE_OPCODE:   0,
+	ENDING_OPCODE: 0,
+}
+
+var dest = map[int]int{
+	ADD_OPCODE:    2,
+	MULT_OPCODE:   2,
+	SAVE_OPCODE:   0,
+	ENDING_OPCODE: 0,
+}
+
+func Exec(program []int) ([]int, error) {
 	result := make([]int, len(program))
 	copy(result, program)
 	i := 0
@@ -36,47 +51,36 @@ func Exec(program []int) []int {
 
 		opModes := make([]int, arity[opcode])
 		for j := 0; j < arity[opcode]; j++ {
-			opModes = append(opModes, param_mode%10)
+			opModes[j] = param_mode % 10
 			param_mode /= 10
 		}
 
-		// TODO: need to remove repetition between add/mult
+		ops := make([]int, args[opcode])
+		for j := 0; j < args[opcode]; j++ {
+			switch opModes[j] {
+			case MODE_POSITION:
+				ops[j] = result[result[i+1+j]]
+			case MODE_IMMEDIATE:
+				ops[j] = result[i+1+j]
+			}
+		}
+
+		destParam := dest[opcode]
+		if destParam > 0 && opModes[destParam] == MODE_IMMEDIATE {
+			return result, errors.New("specified MODE_IMMEDIATE for destination")
+		}
 
 		switch opcode {
 		case ADD_OPCODE:
-			ops := make([]int, 0)
-			for j := 0; j < 2; j++ {
-				switch opModes[i] {
-				case MODE_POSITION:
-					ops = append(ops, result[result[i+1+j]])
-				case MODE_IMMEDIATE:
-					ops = append(ops, result[i+1+j])
-				}
-			}
-			if opModes[2] == MODE_IMMEDIATE {
-				panic("Invalid mode")
-			}
 			dest := result[i+3]
 			result[dest] = ops[0] + ops[1]
 		case MULT_OPCODE:
-			ops := make([]int, 0)
-			for j := 0; j < 2; j++ {
-				switch opModes[i] {
-				case MODE_POSITION:
-					ops = append(ops, result[result[i+1+j]])
-				case MODE_IMMEDIATE:
-					ops = append(ops, result[i+1+j])
-				}
-			}
-			if opModes[2] == MODE_IMMEDIATE {
-				panic("Invalid mode")
-			}
 			dest := result[i+3]
 			result[dest] = ops[0] * ops[1]
 		case SAVE_OPCODE:
 			// IDK
 		case ENDING_OPCODE:
-			return result
+			return result, nil
 		default:
 			log.Panicf("Invalid opcode: %d", opcode)
 		}
--- intcode/intcode_test.go
+++ intcode/intcode_test.go
@@ -1,15 +1,41 @@
 package intcode
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIntcodeExec(t *testing.T) {
-	assert.Equal(t, []int{2, 0, 0, 0, 99}, Exec([]int{1, 0, 0, 0, 99}))
-	assert.Equal(t, []int{2, 3, 0, 6, 99}, Exec([]int{2, 3, 0, 3, 99}))
-	assert.Equal(t, []int{2, 4, 4, 5, 99, 9801}, Exec([]int{2, 4, 4, 5, 99, 0}))
-	assert.Equal(t, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, Exec([]int{1, 1, 1, 4, 99, 5, 6, 0, 99}))
-	assert.Equal(t, []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, Exec([]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}))
+	result, err := Exec([]int{1, 0, 0, 0, 99})
+	assert.Equal(t, []int{2, 0, 0, 0, 99}, result)
+	assert.Nil(t, err)
+
+	result, err = Exec([]int{2, 3, 0, 3, 99})
+	assert.Equal(t, []int{2, 3, 0, 6, 99}, result)
+	assert.Nil(t, err)
+
+	result, err = Exec([]int{2, 4, 4, 5, 99, 0})
+	assert.Equal(t, []int{2, 4, 4, 5, 99, 9801}, result)
+	assert.Nil(t, err)
+
+	result, err = Exec([]int{1, 1, 1, 4, 99, 5, 6, 0, 99})
+	assert.Equal(t, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, result)
+	assert.Nil(t, err)
+
+	result, err = Exec([]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50})
+	assert.Equal(t, []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, result)
+	assert.Nil(t, err)
+}
+
+func TestParamMode(t *testing.T) {
+	result, err := Exec([]int{1002, 4, 3, 4, 33})
+
+	assert.Equal(t, []int{1002, 4, 3, 4, 99}, result)
+	assert.Nil(t, err)
+
+	_, err = Exec([]int{11002, 4, 3, 4, 33})
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New("specified MODE_IMMEDIATE for destination"), err)
 }
