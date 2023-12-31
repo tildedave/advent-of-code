fd3c9dfdc60812d809e2b636487132a7ef2e3393
Dave King <tildedave@gmail.com>
2023-12-30 20:21:40 -0500

    Day 13: second star.  yes it is that dumb.

--- day13/day13.go
+++ day13/day13.go
@@ -11,7 +11,7 @@ import (
 	"seehuhn.de/go/ncurses"
 )
 
-const LOOP_DELAY = 750
+const LOOP_DELAY = 2
 
 // observed size of grid from running program.
 const X_MAX = 40
@@ -52,86 +52,8 @@ func computePaddleMove(
 	paddlePosition [2]int,
 	lastBallPosition [2]int,
 	ballPosition [2]int) int {
-	dx := ballPosition[0] - lastBallPosition[0]
-	dy := ballPosition[1] - lastBallPosition[1]
-	// I guess assume the ball just runs forward until it reaches something.
-	// if it's heading towards the padding (dy position)
-	// I guess it could hit the edge of the screen too, in which case dx/dy
-	// would change.
-	// hitting a block destroys the block and does a dx / dy thing.
-	// we only have to destroy every block, we don't have to be super efficient
-	// I suppose.  guess we'll see how much logic I need to program into this
-	// to hit it at an edge or whatever.
-
-	x, y := ballPosition[0], ballPosition[1]
-	var path string
-	willCollideWithPaddle := false
-	numIterations := 0
-
-	for y < Y_MAX {
-		x += dx
-		y += dy
-
-		path += fmt.Sprintf("(%d, %d) ", x, y)
-
-		// see what's at the location.
-		// I guess we can be sort of dumb and not care about removing the
-		// blocks here.
-		// assumes dx/dy are always in the range [-1, 1]
-		potentialXCollision := [2]int{x + dx, y}
-		potentialYCollision := [2]int{x, y + dy}
-
-		for n, collider := range [][2]int{potentialXCollision, potentialYCollision} {
-			switch tiles[key(collider[0], collider[1])] {
-			case 0:
-				// nothing
-			case 1:
-				// wall.  based on if we're at x max, y max, whatever, we reverse.
-				if x == 0 || x == X_MAX {
-					dx = -dx
-				}
-				if y == 0 {
-					dy = -dy
-				}
-				path += "[wall] "
-			case 2:
-				// a block.  based on this being an x-collision or a y-collision
-				// we change trajectory.
-				// it is valid that we could collide with both in which case we
-				// should end up just reversing both dx/dy.
-
-				var collisionType string
-				if n == 0 {
-					collisionType = "x"
-					dx = -dy
-				} else {
-					collisionType = "y"
-					dy = -dy
-				}
-				path += fmt.Sprintf("[%s-block] ", collisionType)
-			case 3:
-				willCollideWithPaddle = true
-				path += "[paddle]"
-			case 4:
-				// the same as nothing as we're assuming only one ball.
-			}
-		}
-
-		if willCollideWithPaddle {
-			break
-		}
-		numIterations++
-		if numIterations > 5 {
-			path += "[max]"
-			break
-		}
-	}
-
-	win.MvAddStr(28, 0, fmt.Sprintf("Expected ball path: %s", path))
-	win.MvAddStr(33, 0, path)
-	win.MvAddStr(29, 0, fmt.Sprintf("Paddle position: (%d, %d)", paddlePosition[0], paddlePosition[1]))
-	win.MvAddStr(30, 0, fmt.Sprintf("Expected final ball position: (%d, %d)", x, y))
-	if willCollideWithPaddle || paddlePosition[0] == x {
+	x, _ := ballPosition[0], ballPosition[1]
+	if paddlePosition[0] == x {
 		return 0
 	}
 
@@ -166,6 +88,7 @@ func Run(f *os.File, partTwo bool) {
 
 	gameHasStarted := false
 	var wg sync.WaitGroup
+	var score int
 	wg.Add(2)
 	go func() {
 		intcode.ExecFull(program, input, output)
@@ -174,7 +97,10 @@ func Run(f *os.File, partTwo bool) {
 	}()
 	go func() {
 		ncurses.Init()
-		defer ncurses.EndWin()
+		defer func() {
+			ncurses.EndWin()
+			fmt.Printf("Final score: %d\n", score)
+		}()
 		win := ncurses.NewWin(35, 120, 0, 0)
 		var ballPosition [2]int
 		var paddlePosition [2]int
@@ -202,6 +128,7 @@ func Run(f *os.File, partTwo bool) {
 					win.MvAddStr(y, x, StringForTileId(tileId))
 				} else {
 					win.MvAddStr(27, 0, fmt.Sprintf("Score %d", tileId))
+					score = tileId
 				}
 
 				// So here we need to understand the trajectory of the ball.
