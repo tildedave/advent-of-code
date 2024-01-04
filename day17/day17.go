73ef19b774660f7494bff05eb18aa3d950bba21b
Dave King <tildedave@gmail.com>
2024-01-03 20:32:57 -0500

    Day 17: some fiddling for part2, need to put it together

--- day17/day17.go
+++ day17/day17.go
@@ -15,10 +15,120 @@ const DEAD_ROBOT = 3  // 'X'
 const ROBOT = 4       // one of ^ < > v
 
 // robot orientation which I am sure I will use in part 2.
-const UP = 1
-const LEFT = 2
-const RIGHT = 3
-const DOWN = 4
+const UP = 0
+const LEFT = 1
+const RIGHT = 2
+const DOWN = 3
+
+type coords = struct {
+	x int
+	y int
+}
+
+type directions = struct {
+	up    bool
+	down  bool
+	left  bool
+	right bool
+}
+
+type robotCommand = struct {
+	steps int // 0 for turns
+	turn  int // 1 = left, 2 = right
+}
+
+func moveInDirection(location coords, direction int) coords {
+	switch direction {
+	case UP:
+		return coords{location.x, location.y - 1}
+	case DOWN:
+		return coords{location.x, location.y + 1}
+	case LEFT:
+		return coords{location.x - 1, location.y}
+	case RIGHT:
+		return coords{location.x + 1, location.y}
+	default:
+		panic("Invalid direction")
+	}
+}
+
+func locationIsNotNode(location coords, nodeCoordsReverse map[coords]int) bool {
+	_, ok := nodeCoordsReverse[location]
+	return !ok
+}
+
+func getTurn(d1, d2 int) int {
+	// return 0 - no turn
+	if d1 == d2 {
+		return 0
+	}
+	if d1 == reverseDirection(d2) {
+		panic("Should be impossible")
+	}
+	if d1 == UP {
+		// d2 must be left or right which match our return values
+		return d2
+	}
+	if d1 == DOWN {
+		// similar to d1 but in reverse.
+		return reverseDirection(d2)
+	}
+	if d1 == LEFT {
+		if d2 == UP {
+			return RIGHT
+		}
+		if d2 == DOWN {
+			return LEFT
+		}
+		panic("Impossible")
+	}
+	if d1 == RIGHT {
+		if d2 == UP {
+			return LEFT
+		}
+		if d2 == DOWN {
+			return RIGHT
+		}
+		panic("Impossible")
+	}
+	// Well, this isn't the best code I've ever written.
+	panic("Impossible")
+}
+
+func commandsToString(commands []robotCommand) string {
+	str := ""
+	for n, c := range commands {
+		if n != 0 {
+			str += ","
+		}
+		if c.turn != 0 {
+			if c.turn == 1 {
+				str += "L"
+			} else if c.turn == 2 {
+				str += "R"
+			}
+		} else {
+			str += fmt.Sprintf("%d", c.steps)
+		}
+	}
+
+	return str
+}
+
+func reverseDirection(direction int) int {
+	switch direction {
+	case UP:
+		return DOWN
+	case DOWN:
+		return UP
+	case LEFT:
+		return RIGHT
+	case RIGHT:
+		return LEFT
+	default:
+		panic("Invalid direction")
+	}
+}
 
 func contentToString(c int) string {
 	switch c {
@@ -29,7 +139,7 @@ func contentToString(c int) string {
 	case DEAD_ROBOT:
 		return "X"
 	case ROBOT:
-		return "🤖"
+		return "R"
 	default:
 		log.Fatalf("Did not recognize item: %d", c)
 		return ""
@@ -91,36 +201,131 @@ func Run(f *os.File, partTwo bool) {
 		fmt.Println()
 	}
 
-	// let's identify the intersections.
-	scaffolds := make([][2]int, 0)
+	if !partTwo {
+		// let's identify the intersections.
+		scaffolds := make([][2]int, 0)
+		for y := 0; y < len(grid); y++ {
+			for x := 0; x < len(grid[0]); x++ {
+				if grid[y][x] != SCAFFOLD {
+					continue
+				}
+				isInvalid := false
+				for _, d := range [][2]int{{0, -1}, {-1, 0}, {0, 1}, {1, 0}} {
+					offsetX, offsetY := x+d[0], y+d[1]
+					if !(offsetX >= 0 && offsetX < len(grid[0]) && offsetY >= 0 && offsetY < len(grid)) {
+						isInvalid = true
+						continue
+					}
+					if grid[offsetY][offsetX] != SCAFFOLD {
+						isInvalid = true
+						continue
+					}
+				}
+				if !isInvalid {
+					scaffolds = append(scaffolds, [2]int{x, y})
+				}
+			}
+		}
+		sum := 0
+		for _, s := range scaffolds {
+			sum += s[0] * s[1]
+		}
+		fmt.Println(sum)
+		return
+	}
+
+	nodeCoords := make(map[int]coords)
+	nodeCoordsReverse := make(map[coords]int)
+	validDirections := make(map[coords][4]bool)
+	nodeNum := 0
+	var robotPosition coords
+
+	// so for each node we store info between the edges along with the path it
+	// took to get there.
 	for y := 0; y < len(grid); y++ {
 		for x := 0; x < len(grid[0]); x++ {
-			if grid[y][x] != SCAFFOLD {
+			if grid[y][x] == ROBOT {
+				robotPosition = coords{x, y}
+			} else if grid[y][x] != SCAFFOLD {
 				continue
 			}
-			isInvalid := false
-			for _, d := range [][2]int{{0, -1}, {-1, 0}, {0, 1}, {0, 1}} {
+			numAdjacent := 0
+			location := coords{x, y}
+			dirs := [4]bool{}
+			for n, d := range [][2]int{{0, -1}, {-1, 0}, {1, 0}, {0, 1}} {
 				offsetX, offsetY := x+d[0], y+d[1]
 				if !(offsetX >= 0 && offsetX < len(grid[0]) && offsetY >= 0 && offsetY < len(grid)) {
-					isInvalid = true
 					continue
 				}
-				if grid[offsetY][offsetX] != SCAFFOLD {
-					isInvalid = true
-					continue
+				if grid[offsetY][offsetX] == SCAFFOLD {
+					numAdjacent++
+					dirs[n] = true
 				}
 			}
-			if !isInvalid {
-				scaffolds = append(scaffolds, [2]int{x, y})
+			fmt.Println("directions from ", location, "are", dirs)
+			validDirections[location] = dirs
+
+			if numAdjacent != 2 {
+				// it's a node
+				nodeCoords[nodeNum] = location
+				nodeCoordsReverse[location] = nodeNum
+				fmt.Printf("!!! node %d discovered at (%d, %d) - %d neighbors\n", nodeNum, x, y, numAdjacent)
+				nodeNum++
 			}
 		}
 	}
-	sum := 0
-	for _, s := range scaffolds {
-		sum += s[0] * s[1]
-	}
-	fmt.Println(sum)
 
+	graphVizString := "digraph G {\n"
+	for node, coords := range nodeCoords {
+		d := validDirections[coords]
+		graphVizString += fmt.Sprintf("\tNode%d [label=\"(%d, %d)\"];\n", node, coords.x, coords.y)
+		for dir, able := range d {
+			if !able {
+				continue
+			}
+
+			// we also need to keep track of HOW this happened, which requires
+			// understanding left or right turns (more helper functions)
+			location := moveInDirection(coords, dir)
+			numSteps := 1
+			path := make([]robotCommand, 0)
+			for locationIsNotNode(location, nodeCoordsReverse) {
+				var nextDir int = -1
+				for validDir, isOk := range validDirections[location] {
+					if isOk && reverseDirection(validDir) != dir {
+						nextDir = validDir
+						break
+					}
+				}
+				if nextDir == -1 {
+					panic("Should be impossible")
+				}
+
+				turn := getTurn(dir, nextDir)
+				if turn == 0 {
+					numSteps++
+				} else {
+					path = append(path, robotCommand{steps: numSteps})
+					// now we turn
+					path = append(path, robotCommand{turn: turn})
+					numSteps = 0
+				}
+				dir = nextDir
+				fmt.Println("moving direction", dir, "from", location)
+				location = moveInDirection(location, dir)
+				fmt.Println("new location", location)
+			}
+			path = append(path, robotCommand{steps: numSteps})
+			fmt.Printf("I connected node %d (%d, %d) with node %d (%d, %d)\n",
+				node, coords.x, coords.y, nodeCoordsReverse[location], location.x, location.y)
+			graphVizString += fmt.Sprintf("\tNode%d -> Node%d [label=\"%s\"];\n", node, nodeCoordsReverse[location], commandsToString(path))
+		}
+
+	}
+	graphVizString += "}\n"
+	// so now we walk along each node until we find another node.
+
+	fmt.Println(robotPosition.x, robotPosition.y, robotDirection)
+	fmt.Println(graphVizString)
 	// is this it?
-	fmt.Println(robotDirection)
 }
