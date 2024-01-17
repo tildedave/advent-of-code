df706bb2664ed401e996aa9bc1144cee17485573
Dave King <tildedave@gmail.com>
2024-01-17 08:35:28 -0500

    Day 2: some cleanup

--- src/advent2022/day2.clj
+++ src/advent2022/day2.clj
@@ -1,8 +1,14 @@
 (ns advent2022.day2
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]))
 
 (def lines
   (line-seq (io/reader (io/resource "input/day2.txt"))))
+
+
+(def winning-map {"X" "Z", "Y" "X", "Z"  "Y"})
+
+(def losing-map (set/map-invert winning-map))
 
 ;; X rock
 ;; Y paper
@@ -10,17 +16,9 @@
 (defn winning-score
   "Returns 6 if p1 (I) win, 0 if p2 wins, 3 if tie"
   [p1 p2]
-  (case [p1 p2]
-    (["X" "X"]) 3
-    (["X" "Y"]) 0
-    (["X" "Z"]) 6
-    (["Y" "X"]) 6
-    (["Y" "Y"]) 3
-    (["Y" "Z"]) 0
-    (["Z" "X"]) 0
-    (["Z" "Y"]) 6
-    (["Z" "Z"]) 3
-    -10000))
+  (cond (= p1 p2) 3
+        (= (winning-map p1) p2) 6
+        (= (winning-map p2) p1) 0))
 
 ;; there's a cooler way to do this where we use the data in the above function to avoid duplicating the information.
 
@@ -28,15 +26,9 @@
 ;; Y paper
 ;; Z scissors
 (defn desired-shape [result shape]
-  (if (= result "Y") shape
-    (case [result shape]
-    ;; X - we need to lose
-      (["X" "X"]) "Z"
-      (["X" "Y"]) "X"
-      (["X" "Z"]) "Y"
-      (["Z" "X"]) "Y"
-      (["Z" "Y"]) "Z"
-      (["Z" "Z"]) "X")))
+  (cond (= result "Y") shape
+        (= result "X") (winning-map shape)
+        (= result "Z") (losing-map shape)))
 
 (def prediction-map {"A" "X", "B" "Y", "C" "Z"})
 
