2bc55a798cd64cb6cf205039a3a876802701da74
Dave King <tildedave@gmail.com>
2024-01-21 13:24:04 -0500

    Day 10: clean up to use reporter/atom pattern

--- src/advent2022/day10.clj
+++ src/advent2022/day10.clj
@@ -14,7 +14,7 @@
 ;; and converts it into a new state.
 
 (use 'clojure.tools.trace)
-(defn process-line [[cycle x-register queue crt-row] instr]
+(defn process-line [[cycle x-register queue] instr]
   ;; this is the list to reduce
   (let [queue (conj queue (case (first instr)
                             :noop [1 :noop]
@@ -22,10 +22,6 @@
         next-cycle (inc cycle)
         program (get queue 0)
         [cycles-left opcode num] program
-        crt-index (mod (dec cycle) 40)
-        next-crt-row (if (<= (abs (- x-register crt-index)) 1)
-                       (assoc crt-row crt-index \#)
-                       (assoc crt-row crt-index \.))
         [next-x next-executing]
           (if (= cycles-left 1)
             ;; process it
@@ -34,35 +30,48 @@
               :addx [(+ x-register num) (subvec queue 1)])
             ;; continue
             [x-register (assoc queue 0 (assoc program 0 (dec cycles-left)))])]
-    [next-cycle next-x next-executing next-crt-row]))
+    [next-cycle next-x next-executing]))
 
 (def test-program (map parse-line lines))
 
-(defn process-program [program]
+(defn process-program [program reporter]
   (loop [cycle 1
          x-register 1
          executing []
-         crt-row []
-         program program
-         x-states []
-         crt-rows []]
+         program program]
     (if (and (empty? program)
-                 (every? (fn [instr] (= (nth instr 1) :noop)) executing))
-      [x-states (conj crt-rows crt-row)]
+             (every? (fn [instr] (= (nth instr 1) :noop)) executing))
+      ()
       (let [[instr & next-program] (or program (list [:noop]))
-            carry-over (= (mod (dec cycle) 40) 0)
-            crt-rows (if carry-over (conj crt-rows crt-row) crt-rows)
-            crt-row (if carry-over  (vec (repeat 40 \space)) crt-row)
-            [next-cycle next-x next-executing next-crt-row] (process-line [cycle x-register executing crt-row] instr)]
-        (recur next-cycle next-x next-executing next-crt-row next-program (conj x-states x-register) crt-rows)))))
-
-(process-program test-program)
+            [next-cycle next-x next-executing] (process-line [cycle x-register executing] instr)]
+        (reporter cycle x-register)
+        (recur next-cycle next-x next-executing next-program)))))
 
 ;; answer to part 1
-(let [[x-history _] (process-program test-program)
-      indexes (map #(+ % 20) (range 0 220 40))]
-  ;; off by 1 for the cycles.
-  (reduce + (map (fn [n] (* n (nth x-history (dec n)))) indexes)))
+(let [total-signal-strength (atom 0)
+      x-state-reporter (fn [cycle x-register]
+                         (if (= (mod (- cycle 20) 40) 0)
+                           (swap! total-signal-strength + (* cycle x-register))
+                           ()))]
+  (process-program test-program x-state-reporter)
+  (deref total-signal-strength))
+
+(defn crt-reporter [ref cycle x-register]
+  (swap! ref
+         (fn [crt-rows [cycle x-register]]
+           (let [carry-over (= (mod (dec cycle) 40) 0)
+                 crt-rows (if carry-over
+                            (conj crt-rows (vec (repeat 40 \space)))
+                            crt-rows)
+                 crt-index (mod (dec cycle) 40)
+                 which-row (dec (count crt-rows))]
+             (if (<= (abs (- x-register crt-index)) 1)
+               (assoc-in crt-rows [which-row crt-index] \#)
+               (assoc-in crt-rows [which-row crt-index] \.))))
+         [cycle x-register]))
+
+(let [crt-rows (atom [])]
+  (process-program test-program (partial crt-reporter crt-rows))
+  (print (string/join "\n" (map string/join (deref crt-rows)))))
 
 ;; answer to part 2
-(print (string/join "\n" (map string/join (last (process-program test-program)))))
