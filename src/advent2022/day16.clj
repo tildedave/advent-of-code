c383afa0d5878d15a596d25426515e6a45f13314
Dave King <tildedave@gmail.com>
2024-01-27 15:18:49 -0500

    Day 16: first star

--- src/advent2022/day16.clj
+++ src/advent2022/day16.clj
@@ -94,18 +94,23 @@
                 open-set (pop open-set)
                 next-locations (->> valves
                                     (remove #(bit-test open-valves (reverse-bit-idx %))))
-                ;; add "standing pat" as a goal-score.
+                ;; add "standing pat" as a valid location, and try to understand its cost.
                 ;; we DON'T have to add it to the queue.
                 stand-pat-node [location open-valves 0]
-                goal-score (assoc goal-score stand-pat-node (+ (goal-score current-node) (calculate-cost open-valves time-left)))
-                came-from (assoc came-from stand-pat-node current-node)
-                ]
+                stand-pat-score (+ (goal-score current-node) (calculate-cost open-valves time-left))
+                should-stand-pat (< stand-pat-score (get goal-score stand-pat-node Integer/MAX_VALUE))
+                goal-score (if should-stand-pat
+                             (assoc goal-score stand-pat-node stand-pat-score)
+                             goal-score)
+                came-from (if should-stand-pat
+                            (assoc came-from stand-pat-node current-node)
+                            came-from)]
             ;; we want to end up calling recur() on this
             ;; I sort of hate this setup, it seems like it forces me to
             ;; make my loop variables destructured so the recur doesn't need to
             ;; un-bind/re-bind.
-            (println "I am at" current-node "score is" (goal-score current-node))
-            ;; it feels like we need to also handle the time-ran out case.
+            ;; (println "I am at" current-node "score is" (goal-score current-node))
+            ;; (println "stand pat node" stand-pat-node "with score" stand-pat-score)
             (recur
              (reduce
               (fn [[open-set goal-score came-from] next-location]
@@ -125,32 +130,21 @@
                   (if (and
                        (>= next-time-left 0)
                        (< tentative-gscore (get goal-score next-node Integer/MAX_VALUE)))
-                    (do (println "going to node" next-node "with cost" cost "(takes" elapsed-time "minutes)")
+                    (do
+                      ;; (println "going to node" next-node "with cost" cost "(takes" elapsed-time "minutes)")
+                      ;; (println "setting goal-score" next-node "to" tentative-gscore)
                     ;; so we add it here
-                        [(assoc open-set next-node tentative-gscore)
-                         (assoc goal-score next-node tentative-gscore)
-                         (assoc came-from next-node current-node)])
+                      [(assoc open-set next-node (+ tentative-gscore (- 30 (flows next-location))))
+                       (assoc goal-score next-node tentative-gscore)
+                       (assoc came-from next-node current-node)])
                     ;; otherwise we don't bother
-                    (do (println "no reason to go to" next-node "- next node score" (get goal-score next-node))
+                    (do
+                      ;; (println "no reason to go to" next-node "- next node score" (get goal-score next-node))
                     [open-set goal-score came-from]))))
               [open-set goal-score came-from]
               next-locations)))))))
 
 (def distances (all-pairs-shortest-paths adjacency-matrix))
-
-;; dusting this off to give our final answer
-(defn score [valve-ordering distances flows]
-  (loop [location "AA"
-         time-left 30
-         score 0
-         locations valve-ordering]
-    (if (empty? locations)
-      score
-      (let [[next-location] locations]
-        (if (< time-left 0) score
-            (let [next-time-left (- time-left 1 (distances [location next-location]))
-                  next-score (+ score (* next-time-left (flows next-location)))]
-              (recur next-location next-time-left next-score (rest locations))))))))
 
 (defn reconstruct-path [node came-from]
   (loop [curr node result []]
@@ -158,11 +152,16 @@
       (recur (came-from curr) (conj result curr))
       (reverse (map first (conj result curr))))))
 
+(- (calculate-cost 0 30) 779)
+
 (let [[goal-score came-from] (search)
       distances (all-pairs-shortest-paths adjacency-matrix)
       true-goal-scores (->> goal-score
                             (filter (fn [[[_ _ time-left]]] (= time-left 0))))
       best-node (first (sort-by second true-goal-scores))
+      worst-score (calculate-cost 0 30)]
+  (- worst-score (second best-node)))
+
       path (drop-last (reconstruct-path (first best-node) came-from))]
   (println best-node)
   (println path)
@@ -170,4 +169,5 @@
 
 ;; correct answer is AA DD BB JJ HH EE CC
 ;; for whatever reason, we choose the wrong one.
+;; also not clear why we don't include EE / CC in the final.
 ;; 1522 cost so far.
