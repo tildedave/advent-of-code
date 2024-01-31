96cc539005838454c979890b298a4b6de810247e
Dave King <tildedave@gmail.com>
2024-01-31 05:02:27 -0500

    Day 18: doesn't work; not detecting adjacent cavities

--- src/advent2022/day18.clj
+++ src/advent2022/day18.clj
@@ -1,5 +1,6 @@
 (ns advent2022.day18
   (:require [advent2022.utils :as utils]
+            [clojure.set :as set]
             [clojure.math.combinatorics :as combo]))
 
 (defn parse-line [str]
@@ -47,4 +48,73 @@
        (filter #(= % 1))
        (count)))
 
+;; answer to part 1
 (surface-area (map parse-line lines))
+
+;; for part 2, we need to find every cube defined by the faces
+;; and see if it was one of the original vertices
+
+;; a vertex that has faces that are totally inside the faces, but
+;; itself is NOT in the list of original vertices.
+
+(defn enclosed-cubes [cubes]
+  (let [cube-set (set cubes)
+        exposed-faces (->> cubes
+                           (reduce count-faces {})
+                           (filter #(= (second %) 1))
+                           (map first)
+                           (set))
+        vertices (apply concat exposed-faces)]
+    (->> vertices
+         (remove (partial contains? cube-set))
+         (map cube-faces)
+         (distinct)
+         (filter (fn [face-vertices] (every? #(contains? exposed-faces %) face-vertices))))))
+
+enclosed-cubes
+
+(enclosed-cubes (map parse-line lines))
+
+(defn count-faces-from-enclosed [acc cube]
+  (->>
+   cube
+   (reduce
+    (fn [acc face]
+      (-> acc
+          (update-in [face] (fnil inc 0))))
+    acc)))
+
+(let [foo (->> (map parse-line lines)
+               (enclosed-cubes)
+               (reduce count-faces-from-enclosed {})
+               (filter #(= (second %) 1)))
+      bar (->> (map parse-line lines)
+           (reduce count-faces {})
+           (filter #(= (second %) 1)))]
+  (println (count foo))
+  (println (count bar))
+  (println "difference" (count (set/difference (set bar) (set foo)))))
+
+(->> (map parse-line lines)
+     (enclosed-cubes)
+     (reduce count-faces-from-enclosed {})
+     (vals)
+     (filter #(= % 1))
+     (count)
+     (- (surface-area (map parse-line lines))))
+
+(- 3498 174)
+
+(reduce count-faces-from-enclosed {} (enclosed-cubes (map parse-line lines)))
+       (vals)
+
+  (count)))
+
+
+
+(defn exposed-surface-area [cubes]
+  (let [area (surface-area cubes)
+        num-enclosed-cubes (enclosed-cubes cubes)]
+    (- area (* num-enclosed-cubes 6))))
+
+(println "exposed surface area" (exposed-surface-area (map parse-line lines)))
