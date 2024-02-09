a4c94075d6df0e6a68aec7b3d8e76ed92d23d465
Dave King <tildedave@gmail.com>
2024-02-09 08:54:42 -0500

    Day 23: memoize elf positions, still wrong

--- src/advent2022/day23.clj
+++ src/advent2022/day23.clj
@@ -8,7 +8,7 @@
 
 (defn parse-grid [lines]
   (->> lines
-     (mapv vec)))
+       (mapv vec)))
 
 (defn valid-coord? [grid [x y]]
   (and (>= y 0)
@@ -18,15 +18,15 @@
 
 (defn move-dir [[x y] dir]
   (let [[dx dy]
-    (case dir
-      :north [0 -1]
-      :south [0 1]
-      :west [-1 0]
-      :east [1 0]
-      :northeast [1 -1]
-      :northwest [-1 -1]
-      :southeast [1 1]
-      :southwest [-1 1])]
+        (case dir
+          :north [0 -1]
+          :south [0 1]
+          :west [-1 0]
+          :east [1 0]
+          :northeast [1 -1]
+          :northwest [-1 -1]
+          :southeast [1 1]
+          :southwest [-1 1])]
     [(+ x dx) (+ y dy)]))
 
 (defn positions-valid? [grid [x y] dir-list]
@@ -36,68 +36,68 @@
 
 (defn has-no-neighbors? [grid [x y]]
   (every?
-  (fn [[x y]] (not= (get-in grid [y x]) \#))
-  (map (partial move-dir [x y])
-       (list :north :northeast :east
-             :southeast :south :southwest
-             :west :northwest))))
+   (fn [[x y]] (not= (get-in grid [y x]) \#))
+   (map (partial move-dir [x y])
+        (list :north :northeast :east
+              :southeast :south :southwest
+              :west :northwest))))
 
-(apply concat '((1) nil (2)))
+(set (apply concat '((1) nil (2))))
 
 (defn elf-positions [grid]
   ;; let's try to make this lazy
-  (apply
-   concat
-   (for [x (range 0 (count (first grid)))
-         y (range 0 (count grid))]
-     (if (= (get-in grid [y x]) \#)
-       (list [x y])
-       nil))))
+  (->> (for [x (range 0 (count (first grid)))
+             y (range 0 (count grid))]
+         (if (= (get-in grid [y x]) \#)
+           (list [x y])
+           nil))
+       (apply concat)
+       (set)))
 
-(defn round-one [grid dir-idx]
+(defn round-one [grid elf-pos-list dir-idx]
   ;; so here we're going to keep track of, for each location, which elves
   ;; propose moving to it. that will make part 2 easy.
   ;; OK, create a bunch of maps and merge-with.
-  (let [elf-pos-list (elf-positions grid)]
-    (apply
-     merge-with concat
-     (for [[x y] elf-pos-list]
-       (if
-        (has-no-neighbors? grid [x y]) nil
-          (loop [i dir-idx
-                 n 0]
-          (cond
-            (> n 4) nil
-            (= i 0)
-            (if (positions-valid? grid [x y] '(:north :northeast :northwest))
-              {(move-dir [x y] :north) [[x y]]}
-              (recur (mod (inc i) 4) (inc n)))
-            (= i 1)
-            (if (positions-valid? grid [x y] '(:south :southeast :southwest))
-              {(move-dir [x y] :south) [[x y]]}
-              (recur (mod (inc i) 4) (inc n)))
-            (= i 2)
-            (if (positions-valid? grid [x y] '(:west :northwest :southwest))
-              {(move-dir [x y] :west) [[x y]]}
-              (recur (mod (inc i) 4) (inc n)))
-            (= i 3)
-            (if (positions-valid? grid [x y] '(:east :northeast :southeast))
-              {(move-dir [x y] :east) [[x y]]}
-              (recur (mod (inc i) 4) (inc n)))
-            )))))))
+  (apply
+   merge-with concat
+   (for [[x y] elf-pos-list]
+     (if
+      (has-no-neighbors? grid [x y]) nil
+      (loop [i dir-idx
+             n 0]
+        (cond
+          (> n 4) nil
+          (= i 0)
+          (if (positions-valid? grid [x y] '(:north :northeast :northwest))
+            {(move-dir [x y] :north) [[x y]]}
+            (recur (mod (inc i) 4) (inc n)))
+          (= i 1)
+          (if (positions-valid? grid [x y] '(:south :southeast :southwest))
+            {(move-dir [x y] :south) [[x y]]}
+            (recur (mod (inc i) 4) (inc n)))
+          (= i 2)
+          (if (positions-valid? grid [x y] '(:west :northwest :southwest))
+            {(move-dir [x y] :west) [[x y]]}
+            (recur (mod (inc i) 4) (inc n)))
+          (= i 3)
+          (if (positions-valid? grid [x y] '(:east :northeast :southeast))
+            {(move-dir [x y] :east) [[x y]]}
+            (recur (mod (inc i) 4) (inc n)))))))))
 
-(defn round-two [grid round-one-results]
+(defn round-two [grid elf-pos-list dir-idx round-one-results]
   ;; for these elves modify the grid.  remove the old one, add the new one.
   (reduce
-   (fn [grid [[nx ny] [[ox oy]]]]
-     (-> grid
+   (fn [[grid elf-pos-list dir-idx] [[nx ny] [[ox oy]]]]
+     [(-> grid
          (assoc-in [ny nx] \#)
-         (assoc-in [oy ox] \.)))
-   grid
+         (assoc-in [oy ox] \.))
+      (-> elf-pos-list
+          (disj [ox oy])
+          (conj [nx ny]))
+      dir-idx])
+   [grid elf-pos-list (mod (inc dir-idx) 4)]
    (->> round-one-results
         (filter #(= (count (second %)) 1)))))
-
-(round-one (parse-grid smaller-example-lines) 0)
 
 (defn print-grid [grid]
   (println (string/join "\n" (map string/join grid))))
@@ -127,10 +127,15 @@
 (empty-tiles (parse-grid smaller-example-lines))
 
 (-> (iterate
-     (fn [[grid dir-idx]]
-       [(round-two grid (round-one grid dir-idx)) (mod (inc dir-idx) 4)])
-     [(parse-grid example-lines) 0])
+     (fn [[grid elf-pos-list dir-idx]]
+       (round-two
+        grid elf-pos-list dir-idx
+        (round-one grid elf-pos-list dir-idx)))
+     [(parse-grid input-lines)
+      (elf-positions (parse-grid input-lines))
+      0])
     (nth 10)
     (first)
     (prune-to-elves)
-    (print-grid))
+    (empty-tiles)
+    )
