469913de2fb068f6f6b47a90369dd6fe70b4b5bf
Dave King <tildedave@gmail.com>
2024-02-09 20:11:38 -0500

    Day 23: first star

--- src/advent2022/day23.clj
+++ src/advent2022/day23.clj
@@ -6,15 +6,23 @@
 (def smaller-example-lines (utils/read-resource-lines "input/day23-example2.txt"))
 (def input-lines (utils/read-resource-lines "input/day23.txt"))
 
-(defn parse-grid [lines]
-  (->> lines
-       (mapv vec)))
+;; we actually need this to be an infinite grid.
+;; so we will make it an associative array.
+;; most logic should stay the same.
 
-(defn valid-coord? [grid [x y]]
-  (and (>= y 0)
-       (< y (count grid))
-       (>= x 0)
-       (< x (count (first grid)))))
+(defn bounds [grid]
+  (let [ymin (reduce min (keys grid))
+        ymax (reduce max (keys grid))
+        xmin (reduce min (map #(reduce min (keys %)) (vals grid)))
+        xmax (reduce max (map #(reduce max (keys %)) (vals grid)))]
+    [[xmin xmax] [ymin ymax]]))
+
+(defn parse-grid [lines]
+  (into {}
+        (->> lines
+             (map #(map-indexed vector %))
+             (map #(into {} %))
+             (map-indexed vector))))
 
 (defn move-dir [[x y] dir]
   (let [[dx dy]
@@ -31,7 +39,7 @@
 
 (defn positions-valid? [grid [x y] dir-list]
   (every?
-   (fn [[x y]] (= (get-in grid [y x]) \.))
+   (fn [[x y]] (not= (get-in grid [y x]) \#))
    (map (partial move-dir [x y]) dir-list)))
 
 (defn has-no-neighbors? [grid [x y]]
@@ -42,17 +50,16 @@
               :southeast :south :southwest
               :west :northwest))))
 
-(set (apply concat '((1) nil (2))))
-
 (defn elf-positions [grid]
   ;; let's try to make this lazy
-  (->> (for [x (range 0 (count (first grid)))
-             y (range 0 (count grid))]
-         (if (= (get-in grid [y x]) \#)
-           (list [x y])
-           nil))
-       (apply concat)
-       (set)))
+  (let [[[xmin xmax] [ymin ymax]] (bounds grid)]
+    (->> (for [x (range xmin (inc xmax))
+               y (range ymin (inc ymax))]
+           (if (= (get-in grid [y x]) \#)
+             (list [x y])
+             nil))
+         (apply concat)
+         (set))))
 
 (defn round-one [grid elf-pos-list dir-idx]
   ;; so here we're going to keep track of, for each location, which elves
@@ -66,7 +73,7 @@
       (loop [i dir-idx
              n 0]
         (cond
-          (> n 4) nil
+          (>= n 4) nil
           (= i 0)
           (if (positions-valid? grid [x y] '(:north :northeast :northwest))
             {(move-dir [x y] :north) [[x y]]}
@@ -84,58 +91,115 @@
             {(move-dir [x y] :east) [[x y]]}
             (recur (mod (inc i) 4) (inc n)))))))))
 
+(defn new-row [grid]
+  (let [max-width (->> grid
+                       (vals)
+                       (map count)
+                       (reduce max))]
+    (into {} (map-indexed vector (repeat max-width \.)))))
+
+(assoc-in
+ {0 {0 \., 1 \., 2 \., 3 \., 4 \#, 5 \., 6 \.}, 1 {0 \., 1 \., 2 \#, 3 \#, 4 \#, 5 \., 6 \#}, 2 {0 \#, 1 \., 2 \., 3 \., 4 \#, 5 \., 6 \#}, 3 {0 \., 1 \#, 2 \., 3 \., 4 \., 5 \#, 6 \#}, 4 {0 \#, 1 \., 2 \#, 3 \#, 4 \#, 5 \., 6 \.}, 5 {0 \#, 1 \#, 2 \., 3 \#, 4 \., 5 \#, 6 \#}, 6 {0 \., 1 \#, 2 \., 3 \., 4 \#, 5 \., 6 \.}}
+ [5 -1]
+ \d)
+
+
 (defn round-two [grid elf-pos-list dir-idx round-one-results]
   ;; for these elves modify the grid.  remove the old one, add the new one.
   (reduce
    (fn [[grid elf-pos-list dir-idx] [[nx ny] [[ox oy]]]]
-     [(-> grid
-         (assoc-in [ny nx] \#)
-         (assoc-in [oy ox] \.))
-      (-> elf-pos-list
-          (disj [ox oy])
-          (conj [nx ny]))
-      dir-idx])
+     (let [grid (if (not (contains? grid ny))
+                  (assoc grid ny (new-row grid))
+                  grid)]
+       [(-> grid
+            (assoc-in [ny nx] \#)
+            (assoc-in [oy ox] \.))
+        (-> elf-pos-list
+            (disj [ox oy])
+            (conj [nx ny]))
+        dir-idx]))
    [grid elf-pos-list (mod (inc dir-idx) 4)]
    (->> round-one-results
         (filter #(= (count (second %)) 1)))))
 
-(defn print-grid [grid]
-  (println (string/join "\n" (map string/join grid))))
+(assoc-in (parse-grid example-lines) [0 0] \d)
 
+(defn string-grid [grid]
+  (let [[[xmin xmax] [ymin ymax]] (bounds grid)]
+    (string/join
+     "\n"
+     (for [y (range ymin (inc ymax))]
+       (let [row (for [x (range xmin (inc xmax))]
+                   (let [ch (get-in grid [y x] \.)]
+                     ch))]
+         (string/join row))))))
+
+
+(defn print-grid [grid]
+  (println (string-grid grid)))
+
+;; needs to get rewritten to for
 (defn empty-tiles [grid]
   (->> grid
-       (map (fn [l] (filter #(= % \.) l)))
-       (map count)
-       (reduce + 0)))
+      (string-grid)
+      (filter #(= \. %))
+       (count)))
 
 (defn prune-to-elves [grid]
+  (println "prune to elves" (print-grid grid))
   ;; first elf, last elf in each row.
-  (let [elf-bounds (map #(vector (.indexOf % \#) (.lastIndexOf % \#)) grid)
-        x-min-bounds (remove #(= % -1) (map first elf-bounds))
-        x-max-bounds (map second elf-bounds)
+  (let [elf-bounds
+        (map
+         (fn [[k v-list]]
+           (let [elf-pos (->> v-list
+                              (filter #(= (second %) \#))
+                              (map first))]
+             (if (empty? elf-pos)
+               [k nil]
+               [k [(reduce min elf-pos) (reduce max elf-pos)]])))
+         grid)
+        x-min-bounds (->> elf-bounds
+                          (map second)
+                          (remove nil?)
+                          (map first))
+        x-max-bounds (->> elf-bounds
+                          (map second)
+                          (remove nil?)
+                          (map second))
         x-min (reduce min x-min-bounds)
         x-max (reduce max x-max-bounds)
         ;; for y-min / y-max we do a map-indexed on the bounds and see which
         ;; is the first/last  one that's not -1 -1]
-        y-bounds (remove #(= (first (second %)) -1) (map-indexed vector elf-bounds))
-        [y-min] (first y-bounds)
-        [y-max] (last y-bounds)]
-    [[x-min x-max] [y-min y-max]]
-    (map #(subvec % x-min (inc x-max))
-         (subvec grid y-min (inc y-max)))))
+        y-bounds (->> elf-bounds
+                      (remove (fn [[_ v-list]] (nil? v-list)))
+                      (map first)
+                      (sort))
+        y-min (first y-bounds)
+        y-max (last y-bounds)]
+    ;; [[x-min x-max] [y-min y-max]]
+    (->> grid
+         (remove (fn [[y _]] (or (< y y-min) (> y y-max))))
+         (map (fn [[y v-list]]
+                [y (into {} (remove (fn [[x _]] (or (< x x-min) (> x x-max))) v-list))]))
+         (into {}))))
 
-(empty-tiles (parse-grid smaller-example-lines))
+(prune-to-elves (parse-grid smaller-example-lines))
 
 (-> (iterate
      (fn [[grid elf-pos-list dir-idx]]
        (round-two
         grid elf-pos-list dir-idx
         (round-one grid elf-pos-list dir-idx)))
-     [(parse-grid input-lines)
-      (elf-positions (parse-grid input-lines))
+     [(parse-grid example-lines)
+      (elf-positions (parse-grid example-lines))
       0])
     (nth 10)
     (first)
     (prune-to-elves)
-    (empty-tiles)
-    )
+    (empty-tiles))
+    ;; (empty-tiles)
+    ;; (println "tiles"))
+    ;; (prune-to-elves)
+    ;; (print-grid)
+    ;; (empty-tiles)
+
