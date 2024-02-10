3ab65257b6d7213bf660423a94593a70e177f4a7
Dave King <tildedave@gmail.com>
2024-02-10 14:00:52 -0500

    Day 1: both stars

---
+++ src/advent2021/day1.clj
@@ -0,0 +1,29 @@
+(ns advent2021.day1
+  (:require [advent2021.utils :as utils]))
+
+(def example-lines (utils/read-resource-lines "input/day1-example.txt"))
+(def input-lines (utils/read-resource-lines "input/day1.txt"))
+
+(defn answer-part1 [lines]
+  (let [parsed-lines (map utils/parse-int lines)]
+  (->> (map vector parsed-lines (rest parsed-lines))
+       (filter #(> (second %) (first %)))
+       (count))))
+
+(answer-part1 example-lines)
+(answer-part1 input-lines)
+
+(defn answer-part2 [lines]
+  (let [parsed-lines (map utils/parse-int lines)
+        merged-lines  (loop [lines parsed-lines
+                            result []]
+                       (if (< (count lines) 3) result
+                           (recur
+                            (rest lines)
+                            (conj result (reduce + (take 3 lines))))))]
+    (->>
+     (map vector merged-lines (rest merged-lines))
+     (filter #(> (second %) (first %)))
+     (count))))
+
+(answer-part2 input-lines)
---
+++ src/advent2021/utils.clj
@@ -0,0 +1,27 @@
+(ns advent2021.utils
+  (:require [clojure.java.io :as io]))
+
+(defn read-resource-lines [resource]
+  (line-seq (io/reader (io/resource resource))))
+
+(defn parse-int [str] (Integer/valueOf str))
+(defn try-parse-int [str]
+  (try
+    (Integer/valueOf str)
+    (catch IllegalArgumentException _ str)))
+
+
+;; https://github.com/clojure/core.incubator/blob/be509fd967df8ce1ee43c43bca52360cf710252a/src/main/clojure/clojure/core/incubator.clj#L63-L75
+(defn dissoc-in
+  " Dissociates an entry from a nested associative structure returning a new
+nested structure. keys is a sequence of keys. Any empty maps that result
+will not be present in the new structure. "
+  [m [k & ks :as keys]]
+  (if ks
+    (if-let [nextmap (get m k)]
+      (let [newmap (dissoc-in nextmap ks)]
+        (if (seq newmap)
+          (assoc m k newmap)
+          (dissoc m k)))
+      m)
+    (dissoc m k)))
