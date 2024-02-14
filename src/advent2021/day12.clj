45dd1c3f60357965e5935380681229ae98e9a5c7
Dave King <tildedave@gmail.com>
2024-02-13 21:40:08 -0500

    Day 12: both stars with dynamic :-) :troll:

--- src/advent2021/day12.clj
+++ src/advent2021/day12.clj
@@ -24,29 +24,33 @@
                    (keys)
                    (map-indexed vector))))))
 
-(defn neighbors [adj [current small-cave-bs path]]
+(def ^:dynamic part2? false)
+
+(defn neighbors [adj [current small-cave-bs single-cave path]]
   (let [idx (bitset-idx adj)
         reverse-idx (set/map-invert idx)]
     (remove
      nil?
      (for [neighbor (adj current)]
        (cond
-         (= neighbor "end") [neighbor small-cave-bs (cons neighbor path)]
+         (= neighbor "end") [neighbor small-cave-bs single-cave (cons neighbor path)]
          (= neighbor "start") nil
          (small-cave? neighbor)
          (let [neighbor-idx (reverse-idx neighbor)]
            (if (bit-test small-cave-bs neighbor-idx)
-             nil
-             [neighbor (bit-set small-cave-bs neighbor-idx) (cons neighbor path)]))
+             (if (and (nil? single-cave) part2?)
+               [neighbor small-cave-bs neighbor (cons neighbor path)]
+               nil)
+             [neighbor (bit-set small-cave-bs neighbor-idx) single-cave (cons neighbor path)]))
          :else
-         [neighbor small-cave-bs (cons neighbor path)])))))
+         [neighbor small-cave-bs single-cave (cons neighbor path)])))))
 
 (defn dfs [adj]
-  (loop [[queue visited paths] [[["start" 0 '("start")]] #{} #{}]]
+  (loop [[queue visited paths] [[["start" 0 nil '("start")]] #{} #{}]]
     (if (empty? queue)
       paths
       (let [current (first queue)
-            [cave _ path] current]
+            [cave _ _ path] current]
         (if (= cave "end")
           (recur
            [(subvec queue 1)
@@ -73,3 +77,18 @@
 (answer-part1 (utils/read-resource-lines "input/day12-example2.txt"))
 (answer-part1 (utils/read-resource-lines "input/day12-example3.txt"))
 (answer-part1 (utils/read-resource-lines "input/day12.txt"))
+
+
+(defn answer-part2 [lines]
+  ;; I hope this doesn't awaken something in me
+  (binding [part2? true]
+    (->> lines
+       (parse-adjacency)
+       (dfs)
+       (count))))
+
+(answer-part2 (utils/read-resource-lines "input/day12-example.txt"))
+(answer-part2 (utils/read-resource-lines "input/day12-example2.txt"))
+(answer-part2 (utils/read-resource-lines "input/day12-example3.txt"))
+;; this is slow but whatever.
+(answer-part2 (utils/read-resource-lines "input/day12.txt"))
