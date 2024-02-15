e7d4bc62c11c62a9c494c1dc2e9f8a189f7dc8db
Dave King <tildedave@gmail.com>
2024-02-15 06:15:39 -0500

    Day 15: extract A* to common place

--- src/advent2021/day15.clj
+++ src/advent2021/day15.clj
@@ -1,7 +1,6 @@
 (ns advent2021.day15
   (:require [advent2021.utils :as utils])
-  (:require [advent2021.grid :as grid])
-  (:require [clojure.data.priority-map :refer [priority-map]]))
+  (:require [advent2021.grid :as grid]))
 
 ;; we will try A*, again.
 (grid/parse
@@ -15,38 +14,10 @@
 (defn distance [grid [x y] [nx ny]]
   (get-in grid [ny nx]))
 
-;; we search for the end.
-;; cost to get to the end is just our risk.
-;; I am going to extract this to a common function because I'm tired of
-;; programming A* search :-)
-(defn a*-search [start goal neighbors heuristic distance]
-  (loop [[goal-score open-set nodes]
-         [{start 0}
-         (priority-map start 0)
-          0]]
-    (if-let [[current _] (peek open-set)]
-      (cond
-        (= current goal) (goal-score current)
-        (> nodes 1000000) (throw (Exception. "too many nodes"))
-        :else (recur
-        (reduce
-         (fn [[goal-score open-set nodes] neighbor]
-           (let [n-distance (distance current neighbor)
-                 n-new-score (+ (goal-score current) n-distance)
-                 n-score (get goal-score neighbor Integer/MAX_VALUE)]
-             (if (< n-new-score n-score)
-               [(assoc goal-score neighbor n-new-score)
-               (assoc open-set neighbor (+ n-new-score (heuristic neighbor)))
-                nodes]
-               [goal-score open-set nodes])))
-         [goal-score (pop open-set) (inc nodes)]
-         (neighbors current))))
-      (throw (Exception. "could not reach goal")))))
-
 (defn answer-part1 [lines]
   (let [grid (grid/parse lines (fn [ch] (Integer/valueOf (str ch))))
         goal (mapv dec (grid/bounds grid))]
-    (a*-search [0 0] goal
+    (grid/a*-search [0 0] goal
                (partial grid/neighbors grid)
                (partial heuristic grid)
                (partial distance grid))))
@@ -86,16 +57,10 @@
         new-risk (+ (get-in grid [oy ox]) num-tiles)]
     (if (> new-risk 9) (- new-risk 9) new-risk)))
 
-
-(neighbors-part2
- (grid/parse (utils/read-input "day15-example.txt") (fn [ch] (Integer/valueOf (str ch))))
-;;  "abc"
- [9 9])
-
 (defn answer-part2 [lines]
   (let [grid (grid/parse lines (fn [ch] (Integer/valueOf (str ch))))
         goal (mapv (fn [n] (dec (* n 5))) (grid/bounds grid))]
-    (a*-search [0 0] goal
+    (grid/a*-search [0 0] goal
                (partial neighbors-part2 grid)
                (partial heuristic-part2 grid)
                (partial distance-part2 grid))))
--- src/advent2021/grid.clj
+++ src/advent2021/grid.clj
@@ -1,5 +1,5 @@
 (ns advent2021.grid
-  (:require [advent2021.utils :as utils]))
+  (:require [clojure.data.priority-map :refer [priority-map]]))
 
 (defn parse [lines sq-f]
   (->> lines
@@ -31,3 +31,32 @@
     (for [x (range 0 xmax)
           y (range 0 ymax)]
       [x y])))
+
+
+;; we search for the end.
+;; cost to get to the end is just our risk.
+;; I am going to extract this to a common function because I'm tired of
+;; programming A* search :-)
+(defn a*-search [start goal neighbors heuristic distance]
+  (loop [[goal-score open-set nodes]
+         [{start 0}
+         (priority-map start 0)
+          0]]
+    (if-let [[current _] (peek open-set)]
+      (cond
+        (= current goal) (goal-score current)
+        (> nodes 1000000) (throw (Exception. "too many nodes"))
+        :else (recur
+        (reduce
+         (fn [[goal-score open-set nodes] neighbor]
+           (let [n-distance (distance current neighbor)
+                 n-new-score (+ (goal-score current) n-distance)
+                 n-score (get goal-score neighbor Integer/MAX_VALUE)]
+             (if (< n-new-score n-score)
+               [(assoc goal-score neighbor n-new-score)
+               (assoc open-set neighbor (+ n-new-score (heuristic neighbor)))
+                nodes]
+               [goal-score open-set nodes])))
+         [goal-score (pop open-set) (inc nodes)]
+         (neighbors current))))
+      (throw (Exception. "could not reach goal")))))
