55861c5b6555ba059b98845f90563b6e9430e835
Dave King <tildedave@gmail.com>
2024-02-18 20:01:13 -0500

    Day 18: first star

--- src/advent2021/day18.clj
+++ src/advent2021/day18.clj
@@ -215,6 +215,38 @@
 (add-snailfish-num [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
                    [7,[[[3,7],[4,3]],[[6,3],[8,8]]]])
 
+(reduce add-snailfish-num
+        [[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+         [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+         [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+         [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+         [7,[5,[[3,8],[1,4]]]]
+         [[2,[2,2]],[8,[8,1]]]
+         [2,9]
+         [1,[[[9,3],9],[[9,0],[0,7]]]]
+         [[[5,[7,4]],7],1]
+         [[[[4,2],2],6],[8,7]]])
+
+
+(defn magnitude [num]
+  (cond
+    (vector? num)
+    (+ (* 3 (magnitude (first num)))
+       (* 2 (magnitude (second num))))
+    (number? num)
+    num))
+
+(defn answer-part1 [lines]
+  (->> lines
+     (map read-string)
+     (reduce add-snailfish-num)
+     (magnitude)))
+
+(answer-part1 (utils/read-input "day18-example.txt"))
+(answer-part1 (utils/read-input "day18.txt"))
+
+(reduce add-snailfish-num (map ))
+
 (def z [[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]] [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]])
 
 (iterate #(first (reduce-snailfish-num-step %)) z)
