e50a7114f5fe66b6c6a1a46355d1648c7e4ec30d
Dave King <tildedave@gmail.com>
2024-03-08 09:36:12 -0500

    [2020] Day 3 - second star

--- src/advent2020/day3.clj
+++ src/advent2020/day3.clj
@@ -3,10 +3,8 @@
             [utils :as utils]))
 
 
-(defn answer-part1 [filename]
-  (let [[dx dy] [3 1]
-        grid (grid/parse (utils/read-input filename))
-        [max-x max-y] (grid/bounds grid)]
+(defn trees-for-slope [grid dx dy]
+  (let [[max-x max-y] (grid/bounds grid)]
     (loop [x 0 y 0 num-trees 0]
       (if (> y max-y)
         num-trees
@@ -17,4 +15,22 @@
            (inc num-trees)
            num-trees))))))
 
+(defn answer-part1 [filename]
+  (let [grid (grid/parse (utils/read-input filename))]
+    (trees-for-slope grid 3 1)))
+
 (answer-part1 "2020/day3-example.txt")
+(answer-part1 "2020/day3.txt")
+
+(defn answer-part2 [filename]
+  (let [grid (grid/parse (utils/read-input filename))]
+    (->> [[1 1]
+          [3 1]
+          [5 1]
+          [7 1]
+          [1 2]]
+         (map #(apply (partial trees-for-slope grid) %))
+         (reduce *))))
+
+(answer-part2 "2020/day3-example.txt")
+(answer-part2 "2020/day3.txt")
