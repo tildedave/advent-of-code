c5d908c06e5e43938703fd8637323a66b82902d5
Dave King <tildedave@gmail.com>
2024-03-08 06:10:57 -0500

    [2020] Day 1 - reduce both parts by factor of O(n)

--- src/advent2020/day1.clj
+++ src/advent2020/day1.clj
@@ -1,25 +1,31 @@
 (ns advent2020.day1
-  (:require [advent2020.utils :as utils]))
+  (:require [advent2020.utils :as utils]
+            [clojure.set :as set]))
 
 
 (defn answer [filename n])
 
+(defn subset-sum [l val]
+  (set/intersection (set l) (->> l (map #(- val %)) (set))))
+
 (defn answer-part1 [filename]
   (let [l (->> (utils/read-input filename)
                (map #(Integer/valueOf %)))]
-    (->> (for [x l y l] [x y])
-         (filter #(< (first %) (second %)))
-         (filter #(= 2020 (+ (first %) (second %))))
-         (first)
-         (reduce *))))
+    (reduce * (subset-sum l 2020))))
+
+(answer-part1 "day1-example.txt")
+(answer-part1 "day1.txt")
 
 (defn answer-part2 [filename]
-(let [l (->> (utils/read-input filename)
-              (map #(Integer/valueOf %)))]
-   (->> (for [x l y l z l] [x y z])
-        (filter #(apply < %))
-        (filter #(= 2020 (apply + %)))
-        (first)
-        (reduce *))))
+  (let [l (->> (utils/read-input filename)
+               (map #(Integer/valueOf %)))]
+     (->>
+      (for [x l]
+       [x (subset-sum l (- 2020 x))])
+      (remove #(empty? (second %)))
+      (first)
+      (#(* (first %) (reduce * (second %))))
+      )))
 
+(answer-part2 "day1-example.txt")
 (answer-part2 "day1.txt")
