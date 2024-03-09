dd76b246218defba3fff148f5b7645ccdebc9def
Dave King <tildedave@gmail.com>
2024-03-09 13:34:39 -0500

    [2020] Day 7: second star

--- src/advent2020/day7.clj
+++ src/advent2020/day7.clj
@@ -31,18 +31,38 @@
 (hash-map 1 2)
 
 
-(let [adj-map (->> (utils/read-input "2020/day7-example.txt")
-                   (map parse-bag)
-                   (reduce into {})
-                   (map (fn [[k v-list]] (map #(hash-map (first %) (list k)) v-list)))
-                   (flatten)
-                   (apply merge-with concat))]
-  (loop [queue ["shiny gold"]
-         valids #{}]
-    (if (empty? queue) valids
-        (let [x (first queue)]
+(defn answer-part1 [filename]
+  (let [adj-map (->> (utils/read-input (format  "2020/%s" filename))
+                     (map parse-bag)
+                     (reduce into {})
+                     (map (fn [[k v-list]] (map #(hash-map (first %) (list k)) v-list)))
+                     (flatten)
+                     (apply merge-with concat))]
+    (->>
+     (loop [queue ["shiny gold"]
+            valids #{}]
+      (if (empty? queue) valids
+          (let [x (first queue)
+                queue (subvec queue 1)
+                next-bags (remove (partial contains? valids) (adj-map x))]
+            (recur
+             (into queue next-bags)
+             (reduce conj valids next-bags)))))
+     (count))))
 
-          )
-        )
+(defn bag-count [adj-map color]
+  (if (empty? (adj-map color))
+    0
+    (reduce
+     +
+     (map #(* (second %) (inc (bag-count adj-map (first %))))
+          (adj-map color)))))
 
+(defn answer-part2 [filename]
+  (let [adj-map (->> (utils/read-input (format  "2020/%s" filename))
+                     (map parse-bag)
+                     (reduce into {}))]
+    (bag-count adj-map "shiny gold")))
 
+(answer-part2 "day7-example.txt")
+(answer-part2 "day7.txt")
