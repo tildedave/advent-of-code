adad05832d4033f5969f3072a28a5387cb286315
Dave King <tildedave@gmail.com>
2024-03-09 13:58:09 -0500

    [2020] Day 8 - second star

--- src/advent2020/day8.clj
+++ src/advent2020/day8.clj
@@ -10,24 +10,45 @@
        (map parse-instruction)
        (vec)))
 
-(defn instr-num [program n]
-  (mod n (count program)))
-
 (defn step [[program acc line-no]]
   (let [[instr arg] (get program line-no)]
     (case instr
-      "nop" [program acc (instr-num program (inc line-no))]
-      "jmp" [program acc (instr-num program (+ line-no arg))]
-      "acc" [program (+ acc arg) (instr-num program (inc line-no))])))
+      "nop" [program acc (inc line-no)]
+      "jmp" [program acc (+ line-no arg)]
+      "acc" [program (+ acc arg) (inc line-no)])))
 
-(defn answer-part1 [filename]
+(defn final-state [program-seq]
   (reduce
-   (fn [seen-lines [_ acc line-no]]
-     (if (contains? seen-lines line-no)
-       (reduced acc)
+   (fn [seen-lines [program acc line-no]]
+     (if (or (contains? seen-lines line-no)
+             (nil? (get program line-no)))
+       (reduced [acc line-no])
        (conj seen-lines line-no)))
    #{}
-   (iterate step [(parse-program filename) 0 0])))
+   program-seq))
+
+(defn answer-part1 [filename]
+  (->> (iterate step [(parse-program filename) 0 0])
+       (final-state)
+       (first)))
 
 (answer-part1 "day8-example.txt")
 (answer-part1 "day8.txt")
+
+(defn answer-part2 [filename]
+  (let [program (parse-program filename)]
+    (loop [instr-no 0]
+      (if (= instr-no (count program))
+        (throw (Exception. "Did not find instruction number"))
+        (let [[instr _] (get program instr-no)
+              state (case instr
+                      "nop" (final-state (iterate step [(assoc-in program [instr-no 0] "jmp") 0 0]))
+                      "jmp" (final-state (iterate step [(assoc-in program [instr-no 0] "nop") 0 0]))
+                      nil)]
+          (cond
+            (nil? state) (recur (inc instr-no))
+            (= (second state) (count program)) (first state)
+            :else (recur (inc instr-no))))))))
+
+(answer-part2 "day8-example.txt")
+(answer-part2 "day8.txt")
