5b75350ee36265667dbdc368d02efce5f148a72a
Dave King <tildedave@gmail.com>
2024-03-10 14:30:30 -0400

    [2020] Day 10 - second star
    It's been a while since I've programmed this but it is still easy. :-)

--- src/advent2020/day10.clj
+++ src/advent2020/day10.clj
@@ -29,3 +29,35 @@
 (answer-part1 "day10-example.txt")
 (answer-part1 "day10-example2.txt")
 (answer-part1 "day10.txt")
+
+(defn last-value [sorted-vec]
+  (+ (get sorted-vec (dec (count sorted-vec))) 3))
+
+(defn next-idxs [sorted-vec idx last-value]
+  (let [v (if (= idx -1) 0 (get sorted-vec idx))]
+    (->> '(1 2 3)
+         (map (partial + idx))
+         (filter #(<= % (count sorted-vec)))
+         (filter #(<= (- (get sorted-vec % last-value) v) 3)))))
+
+(defn count-combinations [sorted-vec]
+  (let [lv (last-value sorted-vec)
+        f (memoize (fn [f idx]
+            (if (= idx (count sorted-vec)) 1
+                            ;; look ahead to the next ones
+              (->> (next-idxs sorted-vec idx lv)
+                   (map (partial f f))
+                   (reduce +)))))
+        f (partial f f)]
+    (f -1)))
+
+(defn answer-part2 [filename]
+  (->> (utils/read-input (format "2020/%s" filename))
+       (map utils/parse-int)
+       (sort)
+       (vec)
+       (count-combinations)))
+
+(answer-part2 "day10-example.txt")
+(answer-part2 "day10-example2.txt")
+(answer-part2 "day10.txt")
