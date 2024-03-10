de1646201e80496a13e4691eaf054af2c223dd75
Dave King <tildedave@gmail.com>
2024-03-09 21:28:48 -0500

    [2020] Day 9 - second star

--- src/advent2020/day9.clj
+++ src/advent2020/day9.clj
@@ -22,3 +22,30 @@
 
 (answer-part1 "day9-example.txt" 5)
 (answer-part1 "day9.txt" 25)
+
+(defn answer-part2 [filename prefix-length]
+  (let [lines (->> (utils/read-input (format "2020/%s" filename))
+                   (map utils/parse-long)
+                   (vec))
+        target (answer-part1 filename prefix-length)
+        [start end] (loop [start 0
+                           end 0
+                           total-sum (get lines 0)]
+                      (cond
+                        (= total-sum target) [start end]
+                        (< total-sum target)
+                        (recur start
+                               (inc end)
+                               (+ total-sum (get lines (inc end))))
+                        (> total-sum target)
+                        (recur (inc start)
+                               end
+                               (- total-sum (get lines start)))))]
+    (->> (range start (inc end))
+         (map #(get lines %))
+         ((fn [s] [(reduce min s) (reduce max s)]))
+         (reduce +)
+         )))
+
+(answer-part2 "day9-example.txt" 5)
+(answer-part2 "day9.txt" 5)
