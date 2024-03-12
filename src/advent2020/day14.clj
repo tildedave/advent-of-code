096cc27ae5177c81d4957f6a316ba8c3e3332f13
Dave King <tildedave@gmail.com>
2024-03-12 09:49:15 -0400

    [2020] Day 14 - second star

--- src/advent2020/day14.clj
+++ src/advent2020/day14.clj
@@ -1,22 +1,54 @@
 (ns advent2020.day14
-  (:require [utils :as utils]))
+  (:require [utils :as utils]
+            [clojure.math.combinatorics :as combo]))
+
+(def ^:dynamic part2? false)
 
 (defn parse-mask [mask-line]
   (let [mask-seq (->> mask-line
                       (seq)
                       (map-indexed (fn [n x]
                                      (vector (- 35 n) x)))
-                      (remove #(= \X (second %)))
-                      (group-by second))]
+                      (group-by second))
+        x-seq (mapv #(bit-shift-left 1 (first %)) (mask-seq \X))]
     (-> mask-seq
         (update-vals #(mapv (fn [n] (bit-shift-left 1 (first n))) %))
         (update-vals #(reduce bit-or %))
-        (update-keys #(case % \0 0 \1 1)))))
+        (update-keys #(case % \0 0 \1 1 \X \X))
+        (assoc :floating-masks x-seq))))
 
 (defn apply-mask [curr-mask val]
-  (-> val
-      (bit-or (curr-mask 1))
-      (bit-and (bit-not (curr-mask 0)))))
+  (if part2?
+    (let [base (-> val
+                   (bit-or (get curr-mask 1 0)))]
+      ;; for each combo of floating masks,
+      ;; do the apply.
+      ;; this will be a for loop that both includes/
+      ;; does not include them.
+      (->> (curr-mask :floating-masks)
+           (map #(vector {0 [%]} {1 [%]}))
+           (apply combo/cartesian-product)
+           (map #(apply merge-with concat %))
+           (map #(update-vals % (fn [s] (reduce bit-or s))))
+           (map (fn [mask]
+                (-> base
+                    (bit-or (get mask 1 0))
+                    (bit-and (bit-not (get mask 0 0))))))
+      ))
+    (-> val
+        (bit-or (get curr-mask 1 0))
+        (bit-and (bit-not (get curr-mask 0 0))))))
+
+(binding [part2? true] (apply-mask {0 68719476684, \X 33, 1 18, :floating-masks [32 1]} 42))
+
+(defn mask-to-bits [m]
+  (loop [i 0 m m res '()]
+    (if (= i 36) res
+        (recur (inc i)
+               (bit-shift-right m 1)
+               (cons (if (bit-test m 0) 1 0) res)))))
+
+;; then we apply EVERY COMBINATION OF floating bit.
 
 (defn parse-line [line]
   (if-let [m (re-matches #"^mask = ((?:0|1|X){36})$" line)]
@@ -27,8 +59,7 @@
 (defn step [[mem mask] [command arg1 arg2]]
   (case command
     :set-mask [mem arg1]
-    :set-mem [(assoc mem arg1 (apply-mask mask arg2)) mask]
-    ))
+    :set-mem [(assoc mem arg1 (apply-mask mask arg2)) mask]))
 
 (defn answer-part1 [filename]
   (->> (utils/read-input (format "2020/%s" filename))
@@ -41,18 +72,23 @@
 (answer-part1 "day14-example.txt")
 (answer-part1 "day14.txt")
 
-  (parse-line "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
+(defn step2 [[mem mask] [command arg1 arg2]]
+  (case command
+    :set-mask [mem arg1]
+    :set-mem [(reduce
+                (fn [mem addr] (assoc mem addr arg2))
+                mem
+                (apply-mask mask arg1))
+              mask]))
 
-(apply-mask (parse-mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") 0)
+(defn answer-part2 [filename]
+  (binding [part2? true]
+    (->> (utils/read-input (format "2020/%s" filename))
+         (map parse-line)
+         (reduce step2 [{} nil])
+         (first)
+         (vals)
+         (reduce +))))
 
-    (-> 11
-        (bit-or (bit-shift-left 1 6))
-        (bit-and
-         (bit-not (bit-shift-left 1 1))))
-
-    (bit-shift-left 1 1)
-
-
-    (map-indexed vector (seq mask-line))
-
-    (utils/read-input "2020/day14-example.txt")
+(answer-part2 "day14-example2.txt")
+(answer-part2 "day14.txt")
