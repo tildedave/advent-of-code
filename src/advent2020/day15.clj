de399ad6cffb29da44b289daa81960ef85217c33
Dave King <tildedave@gmail.com>
2024-03-17 20:48:42 -0400

    [2020] Day 15 - try a "direct" solution (no iterate) to try to improve perf
    Down to 17 seconds

--- src/advent2020/day15.clj
+++ src/advent2020/day15.clj
@@ -26,6 +26,31 @@
 
 (second (nth (game-sequence [0 3 6]) 2020))
 
+(defn solve-direct [starting-numbers stop-idx]
+  (let [starting-history (->> starting-numbers
+                              (map-indexed vector)
+                              (map #(update % 0 inc))
+                              (map (fn [[n x]] [x n]))
+                              (into {}))]
+  (loop
+   [i (inc (count starting-numbers))
+    number-history starting-history
+    say-next 0]
+    (cond
+      (= i stop-idx) say-next
+      (contains? number-history say-next)
+        (recur (inc i)
+               (assoc number-history say-next i)
+               (- i (number-history say-next)))
+      :else
+        (recur
+         (inc i)
+         (assoc number-history say-next i)
+         0)))))
+
+(defn answer-part1-direct [starting-numbers]
+  (solve-direct starting-numbers 2020))
+
 (defn answer-part1 [starting-numbers]
   (-> starting-numbers
       (game-sequence)
@@ -39,16 +64,22 @@
            (first)
            (.split ","))))
 
+
+
 (parse-input "day15.txt")
 
 (answer-part1 [0 3 6])
 (answer-part1 (parse-input "day15.txt"))
+(answer-part1-direct (parse-input "day15.txt"))
 
 (defn number-sequence [game-sequence]
   (map second game-sequence))
 
+
 ;; let's just brute force it.  I've got all day.
+(time (solve-direct (parse-input "day15.txt") 30000000))
 (println (time (nth (number-sequence (game-sequence (parse-input "day15.txt"))) 30000000)))
+
 
 (->> (number-sequence (game-sequence [0 3 6]))
      (map-indexed vector)
