4bbe76ec4f263bae4608454cde5d5ddeabd70569
Dave King <tildedave@gmail.com>
2024-03-20 08:47:29 -0400

    [2020] Day 17 - second star.  nice and simple

--- src/advent2020/day17.clj
+++ src/advent2020/day17.clj
@@ -1,27 +1,22 @@
 (ns advent2020.day17
   (:require [utils :as utils]
-            [grid :as grid]))
+            [grid :as grid]
+            [clojure.math.combinatorics :as combo]))
+
+(def ^:dynamic part2? false)
 
 (defn parse-active-cubes [filename]
   (let [grid-lines (grid/parse (utils/read-input (format "2020/%s" filename)))]
     (->> (grid/coords grid-lines)
                   (filter #(= (grid/at grid-lines %) \#))
-                  (map #(assoc % 2 0))
+                  (map #(into % (if part2? [0 0] [0])))
                   (set))))
 
-(parse-active-cubes "day17-example.txt")
-
-(defn neighbors [[x y z]]
+(defn neighbors [cube]
   (->>
-   (for [dx [-1 0 1]
-        dy [-1 0 1]
-        dz [-1 0 1]]
-    (if (= 0 dx dy dz)
-      nil
-      [(+ x dx) (+ y dy) (+ z dz)]))
-   (remove nil?)))
-
-(count (neighbors [0 0 0]))
+   (for [delta (apply combo/cartesian-product (repeat (count cube) [-1 0 1]))]
+     (map + delta cube))
+   (remove (partial = cube))))
 
 (defn inactivate-cubes [active-cubes]
   (filter
@@ -56,12 +51,17 @@
 
 (activate-cubes #{[1 0 0] [2 2 0] [0 2 0] [1 2 0] [2 1 0]})
 
-(defn answer-part1 [filename]
+(defn answer [filename]
   (->> (parse-active-cubes filename)
        (iterate step)
        (take 7)
        (last)
        (count)))
 
+(defn answer-part1 [filename] (answer filename))
 (answer-part1 "day17-example.txt")
 (answer-part1 "day17.txt")
+
+(defn answer-part2 [filename] (binding [part2? true] (answer filename)))
+(answer-part2 "day17-example.txt")
+(answer-part2 "day17.txt")
