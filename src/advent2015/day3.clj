e314623c5bc2b7ca02a4b0929c28a634b5260cf0
Dave King <tildedave@gmail.com>
2024-04-04 09:23:24 -0400

    [2015] Day 3 - second star.  ugly but it works

--- src/advent2015/day3.clj
+++ src/advent2015/day3.clj
@@ -1,16 +1,18 @@
 (ns advent2015.day3
   (:require [utils :as utils]))
 
+(defn step [[houses-so-far [x y]] ch]
+  (let [[nx ny] (case ch
+                  \^ [x (dec y)]
+                  \v [x (inc y)]
+                  \> [(inc x) y]
+                  \< [(dec x) y])]
+    [(update houses-so-far [nx ny] (fnil inc 0))
+     [nx ny]]))
+
 (defn visit [instructions]
   (reduce
-   (fn [[houses-so-far [x y]] ch]
-     (let [[nx ny] (case ch
-                     \^ [x (dec y)]
-                     \v [x (inc y)]
-                     \> [(inc x) y]
-                     \< [(dec x) y])]
-       [(update houses-so-far [nx ny] (fnil inc 0))
-        [nx ny]]))
+   step
    [{[0 0] 1} [0 0]]
    (seq instructions)))
 
@@ -22,3 +24,21 @@
 
 (answer-part1 "^>v<")
 (answer-part1 (first (utils/read-input "2015/day3.txt")))
+
+(defn visit-p2 [instructions]
+  (reduce
+   (fn [[houses-so-far [sx sy] [rx ry] n] ch]
+     (if (= (mod n 2) 0)
+       (let [[houses-so-far [sx sy]] (step [houses-so-far [sx sy]] ch)]
+         [houses-so-far [sx sy] [rx ry] (inc n)])
+       (let [[houses-so-far [rx ry]] (step [houses-so-far [rx ry]] ch)]
+         [houses-so-far [sx sy] [rx ry] (inc n)])))
+   [{[0 0] 2} [0 0] [0 0] 0]
+   (seq instructions)))
+
+
+(defn answer-part2 [instructions]
+  (->> (visit-p2 instructions)
+       (first)
+       (keys)
+       (count)))
