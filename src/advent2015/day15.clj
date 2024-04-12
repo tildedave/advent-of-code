052a750a0401784979a8928956364cc1f00f2e8d
Dave King <tildedave@gmail.com>
2024-04-12 16:50:04 -0400

    [2015] Day 15 - second star

--- src/advent2015/day15.clj
+++ src/advent2015/day15.clj
@@ -30,29 +30,22 @@
          #(cons n %)
          (breakdowns (- max n) (dec num-ingredients)))))))
 
-(defn score [ingredient-props ingredient-quantity-map]
-  (let [vs (->>
-           (for [[ingredient quantity] ingredient-quantity-map]
-             (->>
-              (for [[k c] (ingredient-props ingredient)]
-                {k (* c quantity)})
-              (apply merge)))
-           (apply merge-with +)
-           (#(dissoc % "calories"))
-           (vals))]
-   (if (empty? (filter #(< % 0) vs))
-     (reduce * vs)
-     0)))
+(defn total-values [ingredient-props quantity-map]
+  (->>
+   (for [[ingredient quantity] quantity-map]
+     (->>
+      (for [[k c] (ingredient-props ingredient)]
+        {k (* c quantity)})
+      (apply merge)))
+   (apply merge-with +)))
 
-;;    (vals)
-;;    (reduce *)))
-
- (let [ingredient-props (->> "2015/day15-example.txt"
-                            (utils/read-input)
-                            (map parse-ingredient)
-                            (reduce merge))]
-   (score ingredient-props {"Butterscotch" 44 "Cinnamon" 56}))
-
+(defn score [vs]
+  (let [vs (-> vs
+               (dissoc "calories")
+               (vals))]
+    (if (empty? (filter #(< % 0) vs))
+      (reduce * vs)
+      0)))
 
 (defn answer-part1 [filename]
   (let [ingredient-props (->> (format "2015/%s" filename)
@@ -65,7 +58,8 @@
                                    (map #(apply merge %))
                                    )]
     (->> ingredient-breakdowns
-         (map (partial score ingredient-props))
+         (map (partial total-values ingredient-props))
+         (map score)
          (sort >)
          (first)
          )))
@@ -73,3 +67,23 @@
 (answer-part1 "day15-example.txt")
 (answer-part1 "day15.txt")
 
+
+(defn answer-part2 [filename]
+  (let [ingredient-props (->> (format "2015/%s" filename)
+                              (utils/read-input)
+                              (map parse-ingredient)
+                              (reduce merge))
+        ingredients (vec (keys ingredient-props))
+        ingredient-breakdowns (->> (breakdowns 100 (count ingredients))
+                                   (map #(map-indexed (fn [n quantity] {(get ingredients n 0) quantity}) %))
+                                   (map #(apply merge %)))]
+    (->> ingredient-breakdowns
+         (map (partial total-values ingredient-props))
+         (filter #(= (get % "calories") 500))
+         (map score)
+         (sort >)
+         )
+  ))
+
+(answer-part2 "day15-example.txt")
+(answer-part2 "day15.txt")
