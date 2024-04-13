64dae76ac4cb61864d122e03405cac851645d870
Dave King <tildedave@gmail.com>
2024-04-12 21:22:04 -0400

    [2015] Day 17 - second star

--- src/advent2015/day17.clj
+++ src/advent2015/day17.clj
@@ -2,27 +2,48 @@
   (:require [clojure.math.combinatorics :as combo]
             [utils :as utils]))
 
-(defn answer-part1 [nums]
+(defn count-possibilities [nums target]
   (let [duplicates (->> nums
                         (map #(hash-map % 1))
                         (reduce (partial merge-with +))
                         (remove (fn [[x n]] (= n 1)))
                         (map #(apply hash-map %))
                         (into {}))]
-    (println nums)
     (->> (combo/subsets nums)
-         (filter #(= (reduce + %) 25))
-         (map #(reduce (fn [[acc already-counted] v]
-                         (if (contains? already-counted v)
-                           [(/ acc (get duplicates v 1))
-                            already-counted]
-                           [(* (get duplicates v 1) acc)
-                            (conj already-counted v)]))
-                       [1 #{}] %))
-         (map first)
-         (reduce +))))
+         (filter #(= (reduce + %) target))
+         (map #(let [[n _] (reduce (fn [[acc already-counted] v]
+                                     (if (contains? already-counted v)
+                                       [(/ acc (get duplicates v 1))
+                                        already-counted]
+                                       [(* (get duplicates v 1) acc)
+                                        (conj already-counted v)]))
+                                   [1 #{}] %)]
+                 [n %])))))
 
-(answer-part1 [20 15 10 5 5])
+(defn answer-part1 [nums target]
+  (->> (count-possibilities nums target)
+       (map first)
+       (reduce +)))
+
+(answer-part1 [20 15 10 5 5] 25)
 
 (answer-part1 (->> (utils/read-input "2015/day17.txt")
-                   (map utils/parse-int)))
+                   (map utils/parse-int))
+              150)
+
+(defn answer-part2 [nums target]
+  (->> (count-possibilities nums target)
+       (group-by #(count (second %)))
+       (seq)
+       (sort-by first)
+       (first)
+       (second)
+       (map first)
+       (reduce +)
+       ))
+
+(answer-part2  [20 15 10 5 5] 25)
+
+(answer-part2 (->> (utils/read-input "2015/day17.txt")
+                   (map utils/parse-int))
+              150)
