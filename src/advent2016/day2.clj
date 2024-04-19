567cd6bc1b93d12d7ec452e3c61ad376f9b063a6
Dave King <tildedave@gmail.com>
2024-04-19 09:13:49 -0400

    [2016] Day 2 - both stars

--- src/advent2016/day2.clj
+++ src/advent2016/day2.clj
@@ -7,24 +7,31 @@
    [4 5 6]
    [7 8 9]])
 
-(defn button-move [[x y] dir]
+(def keypad-p2
+  [[nil nil 1 nil nil]
+   [nil 2 3 4 nil]
+   [5 6 7 8 9]
+   [nil "A" "B" "C" nil]
+   [nil nil "D" nil nil]])
+
+(defn button-move [keypad [x y] dir]
   (let [[nx ny] (case dir
                   \U [x (dec y)]
                   \L [(dec x) y]
                   \R [(inc x) y]
                   \D [x (inc y)])]
-    (if (and (<= 0 nx 2) (<= 0 ny 2))
+    (if (get-in keypad [ny nx])
       [nx ny]
       [x y])))
 
-(defn process-instructions [instr-lists]
-  (loop [[x y] [1 1]
+(defn process-instructions [keypad instr-lists]
+  (loop [[x y] [x y]
          current-instr (first instr-lists)
          rest-instrs (rest instr-lists)
          result []]
     (if-let [instr (first current-instr)]
       (recur
-       (button-move [x y] instr)
+       (button-move keypad [x y] instr)
        (rest current-instr)
        rest-instrs
        result)
@@ -38,12 +45,15 @@
            (rest rest-instrs)
            result))))))
 
-(process-instructions (list (seq "ULL") (seq "RRDDD") (seq "LURDL") (seq "UUUUD")))
+(process-instructions keypad [1 1] (list (seq "ULL") (seq "RRDDD") (seq "LURDL") (seq "UUUUD")))
+
+(process-instructions keypad-p2 [0 2] (list (seq "ULL") (seq "RRDDD") (seq "LURDL") (seq "UUUUD")))
 
 (defn answer-part1 []
   (->> (utils/read-input "2016/day2.txt")
        (map seq)
-       (process-instructions)
-       (string/join "")))
+       (process-instructions keypad [1 1])
+       (string/join)
+       (utils/parse-int)))
 
 (answer-part1)
