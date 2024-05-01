674a0900ea115c72c7741f518f90f9c141a34e1a
Dave King <tildedave@gmail.com>
2024-05-01 12:52:05 -0400

    [2016] Day 22 - second star

--- src/advent2016/day22.clj
+++ src/advent2016/day22.clj
@@ -9,13 +9,13 @@
 (def line-re #"^/dev/grid/node\-x(\d+)\-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T.*$")
 
 (defn parse-line [s]
-    (if-let [[m & l] (re-matches line-re s)]
-      ((fn [[x y size used avail]]
-        {[x y] {:size size
-                :used used
-                :avail avail}})
-      (map utils/parse-int l))
-      {}))
+  (if-let [[m & l] (re-matches line-re s)]
+    ((fn [[x y size used avail]]
+       {[x y] {:size size
+               :used used
+               :avail avail}})
+     (map utils/parse-int l))
+    {}))
 
 (parse-line "/dev/grid/node-x0-y0     91T   66T    25T   72%")
 
@@ -44,87 +44,110 @@
 
 (defn initial-state [filesystem]
   (let [empty (->> filesystem
-                        (filter #(= (:used (second %)) 0))
-                        (first)
-                        (first))
+                   (filter #(= (:used (second %)) 0))
+                   (first)
+                   (first))
         goal (->> filesystem
                   (keys)
                   (filter (fn [[_ y]] (= y 0)))
                   (sort-by first >)
                   (first))]
-    {:filesystem filesystem
-     :empty empty
+    {:empty empty
      :goal goal}))
 
 (defn parse-filesystem [filename]
   (->> (utils/read-input filename)
-        (map parse-line)
-        (reduce merge)))
+       (map parse-line)
+       (reduce merge)))
 
 (defn is-goal? [state]
   (= (:goal state) [0 0]))
 
-(defn adjacent-nodes [[x y] filesystem]
-  (->>
-   (for [[dx dy] [[-1 0] [1 0] [0 1] [0 -1]]]
-     [(+ x dx) (+ y dy)])
-   (filter (fn [spot] (contains? filesystem spot)))
-   (set)))
+(defn adjacent-nodes [filesystem]
+  (fn [[x y]]
+    (->>
+     (for [[dx dy] [[-1 0] [1 0] [0 1] [0 -1]]]
+       [(+ x dx) (+ y dy)])
+     (filter (fn [spot] (contains? filesystem spot)))
+     (set))))
 
-(defn can-copy? [filesystem source dest]
-  (<= (:used (filesystem source)) (:avail (filesystem dest))))
+(defn can-copy? [filesystem]
+  (let [giant-nodes (->> filesystem
+                         (filter #(>= (:size (second %)) 100))
+                         (map first)
+                         (set))]
+  (fn [source dest]
+    (not (contains? giant-nodes dest)))))
+    ;; true))
+    ;; (<= (:used (filesystem source)) (:avail (filesystem dest)))))
 
-;; this is written generically but it seems likely we will only be executing
-;; this in scenarios where dest is 0.
-(defn copy-into [filesystem source dest]
-  (-> filesystem
-      (assoc-in [source :used] 0)
-      (assoc-in [source :avail] (get-in filesystem [source :size]))
-      (assoc-in [dest :used] (get-in filesystem [source :used]))
-      (assoc-in [dest :avail] (- (get-in filesystem [dest :size])
-                                 (get-in filesystem [dest :used])
-                                 (get-in filesystem [source :used])))))
+(defn copy-into [filesystem]
+  (fn [state source dest]
+    (->
+     (if (= source (:goal state))
+       (assoc state :goal dest)
+       state)
+     (assoc :empty source))))
 
-(defn neighbors [state]
+;;      (assoc))
+    ;;  (-> state
+    ;;      (assoc :filesytem (-> filesystem
+    ;;                            (assoc-in [source :used] 0)
+    ;;                            (assoc-in [source :avail] (get-in filesystem [source :size]))
+    ;;                            (assoc-in [dest :used] (get-in filesystem [source :used]))
+    ;;                            (assoc-in [dest :avail] (- (get-in filesystem [dest :size])
+    ;;                                                       (get-in filesystem [dest :used])
+    ;;                                                       (get-in filesystem [source :used])))))
+
+
+(defn neighbors [adjacent-nodes can-copy? copy-into]
+  (fn [state]
   ;; if the goal and the empty are next to each other AND the empty node can
   ;; receive the goal node (I assume this will always be possible)
   ;; otherwise we can move the empty spot around.
   ;; it seems like we need both of these.
-  (let [{:keys [filesystem empty goal]} state]
-    (concat
-     (cond
-       (not (contains? (adjacent-nodes goal filesystem) empty)) '()
-       (not (can-copy? filesystem goal empty)) '()
-     ;; this needs to update the associative structure actually
-       :else (list (-> state
-                       (assoc :filesystem (copy-into filesystem goal empty))
-                       (assoc :empty goal)
-                       (assoc :goal empty))))
-     (->> (disj (adjacent-nodes empty filesystem) goal)
-          (filter #(can-copy? filesystem % empty))
-          (map (fn [node]
-                 (-> state
-                     (assoc :filesystem (copy-into filesystem node empty))
-                     (assoc :empty node))))))))
+    (let [{:keys [empty goal]} state]
+      (concat
+           (cond
+             (not (contains? (adjacent-nodes goal) empty)) '()
+             (not (can-copy? goal empty)) '()
+             :else (list (copy-into state goal empty)))
+           (->> (disj (adjacent-nodes empty) goal)
+                (filter #(can-copy? % empty))
+                (map (fn [node] (copy-into state node empty))))))))
 
-(defn heuristic [{:keys [filesystem empty goal]}]
-  (let [[gx gy] goal
+(defn heuristic [state]
+  (let [{:keys [empty goal]} state
+        [gx gy] goal
         [ex ey] empty]
     (+
      gx
      gy ;; manhattan distance from goal data to start (0)
      (+ (abs (- gx ex)) (+ (abs (- gy ey)))))))
 
-(-> (initial-state (parse-filesystem "2016/day22-example.txt"))
-    (neighbors)
-    (first)
-    (neighbors)
-    (first))
+(let [filesystem (parse-filesystem "2016/day22-example.txt")
+      neighbors (neighbors (adjacent-nodes filesystem) (can-copy? filesystem) (copy-into filesystem))]
+  (-> (initial-state filesystem)
+      (neighbors)
+      (first)
+      (neighbors)))
 
-(grid/a*-search
- (initial-state (parse-filesystem "2016/day22.txt"))
- is-goal?
- neighbors
- heuristic
- (fn [_ _] 1))
+(let [filesystem (parse-filesystem "2016/day22-example.txt")]
+  (grid/a*-search
+   (initial-state filesystem)
+   is-goal?
+   (neighbors (adjacent-nodes filesystem) (can-copy? filesystem) (copy-into filesystem))
+   heuristic
+   (fn [_ _] 1)))
 
+(defn answer-part2 [filename]
+  (let [filesystem (parse-filesystem filename)]
+    (first (grid/a*-search
+     (initial-state filesystem)
+     is-goal?
+     (neighbors (adjacent-nodes filesystem) (can-copy? filesystem) (copy-into filesystem))
+     heuristic
+     (fn [_ _] 1)))))
+
+(answer-part2 "2016/day22-example.txt")
+(time (answer-part2 "2016/day22.txt"))
