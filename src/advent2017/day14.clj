adc6f636dfe41fe824093d8ffe9eccfc60f23a25
Dave King <tildedave@gmail.com>
2024-05-09 21:19:04 -0400

    [2017] Day 14 - second star

--- src/advent2017/day14.clj
+++ src/advent2017/day14.clj
@@ -1,6 +1,8 @@
 (ns advent2017.day14
   (:require [advent2017.day10 :refer [knot-hash]]
-            [utils :as utils]))
+            [utils :as utils]
+            [grid :as grid]
+            [clojure.set :as set]))
 
 ;; probably a better way to do this but I'm tired
 (defn hex-ch-to-binary [ch]
@@ -30,13 +32,13 @@
 (defn grid-coords [input]
   (reduce
    merge
-  (for [n (range 0 128)]
-    (->> (knot-hash (format "%s-%d" input n))
-         (seq)
-         (map hex-ch-to-binary)
-         (reduce concat)
-         (map-indexed (fn [m b] {[m n] b}))
-         (reduce merge)))))
+   (for [n (range 0 128)]
+     (->> (knot-hash (format "%s-%d" input n))
+          (seq)
+          (map hex-ch-to-binary)
+          (reduce concat)
+          (map-indexed (fn [m b] {[m n] b}))
+          (reduce merge)))))
 
 (defn answer [input]
   (->> input
@@ -45,6 +47,37 @@
        (remove zero?)
        (count)))
 
-(answer "flqrgnkx")
-(answer (first(utils/read-input "2017/day14.txt")))
+;; (answer "flqrgnkx")
+;; (answer (first (utils/read-input "2017/day14.txt")))
 ;; (count (remove zero? (vals (grid-coords "flqrgnkx"))))
+
+;; unfortunately the grid logic requires I pass something like this instead of
+;; bounds.
+(def fake-grid (cons (repeat 128 0) (repeat 127 0)))
+
+(def neighbors #(grid/neighbors fake-grid % grid/cardinal-directions))
+
+(defn merge-forests [forests coord]
+  (let [coord-neighbors (set (neighbors coord))
+        [next-forests next]
+        (reduce
+         (fn [[next-forests curr] forest]
+           (if (empty? (set/intersection coord-neighbors forest))
+             [(conj next-forests forest) curr]
+             [next-forests (set/union forest curr)]))
+         [[] #{coord}]
+         forests)]
+    (conj next-forests next)
+    ))
+
+(reduce merge-forests [] [[1 1] [1 2]])
+
+(defn answer-part2 [input]
+  (->> (grid-coords input)
+       (filter (fn [[coords x]] (= x 1)))
+       (map first)
+       (reduce merge-forests [])
+       (count)))
+
+(time (println (answer-part2 "flqrgnkx")))
+(time (println (answer-part2 (first (utils/read-input "2017/day14.txt")))))
