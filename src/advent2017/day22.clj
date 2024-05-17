0059cb0e9b32d62be8a08f0bbedb216724fbd932
Dave King <tildedave@gmail.com>
2024-05-17 12:42:49 -0400

    [2017] Day 22 - second star

--- src/advent2017/day22.clj
+++ src/advent2017/day22.clj
@@ -15,6 +15,11 @@
              :left :up
              :right :down)))
 
+(defn reverse-direction [direction]
+  (->> direction
+       (turn :right)
+       (turn :right)))
+
 (defn move [[x y] direction]
   (let [[dx dy] (case direction
                    :up [0 -1]
@@ -24,39 +29,69 @@
        [(+ x dx) (+ y dy)]))
 
 (defn burst [state]
-  (let [{:keys [direction coord infected]} state
-        direction (turn (if (contains? infected coord) :right :left) direction)
-        action (if (contains? infected coord)
-                 :clean
-                 :infect)]
+  (let [{:keys [direction coord status]} state
+        action (case (get status coord)
+                 :infected :clean
+                 :infect)
+        direction (turn (case (get status coord)
+                          :infected :right
+                          :left) direction)
+        ]
     (-> state
         (assoc :direction direction)
-        (update :infected #(case action
-                             :clean (disj % coord)
-                             :infect (conj % coord)))
+        (update :status #(case action
+                             :clean (dissoc % coord)
+                             :infect (assoc % coord :infected)))
         (assoc :action action)
         (update :coord #(move % direction)))))
 
 (defn initial-state [filename]
   (let [grid (->> (utils/read-input filename)
                   (grid/parse))
-        infected (->> (grid/coords grid)
-                      (filter #(= (grid/at grid %) \#))
-                      (set))
-        _ (println (count grid))]
+        status (->> (grid/coords grid)
+                    (filter #(= (grid/at grid %) \#))
+                    (map #(hash-map % :infected))
+                    (reduce merge))]
     {:coord [(quot (count grid) 2) (quot (count grid) 2)]
      :direction :up
-     :infected infected}))
+     :status status}))
 
-(quot 25 2)
+(initial-state "2017/day22.txt")
 
 (->> (iterate burst (initial-state "2017/day22-example.txt"))
-     (take 10000)
+     (take 10001)
      (filter #(= (:action %) :infect))
      (count))
 
 (initial-state "2017/day22.txt")
 (->> (iterate burst (initial-state "2017/day22.txt"))
-     (take 10000)
+     (take 10001)
+     (filter #(= (:action %) :infect))
+     (count))
+
+(defn burst-part2 [state]
+  (let [{:keys [direction coord status]} state
+        action (case (get status coord :clean)
+                 :clean :weaken
+                 :weakened :infect
+                 :infected :flag
+                 :flagged :clean)
+        direction (case (get status coord :clean)
+                          :clean (turn :left direction)
+                          :weakened direction
+                    :infected (turn :right direction)
+                    :flagged (reverse-direction direction))]
+    (-> state
+        (assoc :direction direction)
+        (update :status #(case action
+                           :clean (dissoc % coord)
+                           :flag (assoc % coord :flagged)
+                           :weaken (assoc % coord :weakened)
+                           :infect (assoc % coord :infected)))
+        (assoc :action action)
+        (update :coord #(move % direction)))))
+
+(->> (iterate burst-part2 (initial-state "2017/day22.txt"))
+     (take 10000001)
      (filter #(= (:action %) :infect))
      (count))
