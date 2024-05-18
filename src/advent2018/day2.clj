a5c15229fc052cad22112d6dc03947bb223d0d19
Dave King <tildedave@gmail.com>
2024-05-17 21:47:23 -0400

    [2018] Day 2 - both stars

---
+++ src/advent2018/day2.clj
@@ -0,0 +1,40 @@
+(ns advent2018.day2
+  (:require [utils :as utils]
+            [clojure.math.combinatorics :as combo]
+            [clojure.string :as string]))
+
+(defn two-or-three [s]
+  (let [s (->> (frequencies s) (vals) (set))]
+    (concat
+     (if (contains? s 2) '(2) '())
+     (if (contains? s 3) '(3) '())
+     '())))
+
+    ;;    (vals)
+    ;;    (filter #(or (= % 2) (= % 3)))
+    ;;    (first)))
+
+(two-or-three "abccdeee")
+
+(->> (utils/read-input "2018/day2.txt")
+     (map two-or-three)
+     (flatten)
+     (remove nil?)
+     (sort)
+     (partition-by identity)
+     (map count)
+     (reduce *))
+
+(->> (combo/combinations (utils/read-input "2018/day2-example2.txt") 2)
+     (map (fn [[s1 s2]]
+            (let [diffs (->> (map = (seq s1) (seq s2))
+                             (remove true?))]
+              (if (= (count diffs) 1)
+                (->> (map vector (seq s1) (seq s2))
+                     (filter #(apply = %))
+                     (map first)
+                     (string/join)
+                     (first)
+                     )
+                nil))))
+     (remove nil?))
