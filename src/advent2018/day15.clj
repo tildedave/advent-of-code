f7867d632652ae8e593e2f92879e39a895dc2711
Dave King <tildedave@gmail.com>
2024-06-03 20:16:53 -0400

    [2018] Day 15 - moving complete

--- src/advent2018/day15.clj
+++ src/advent2018/day15.clj
@@ -27,27 +27,31 @@
   #(grid/neighbors grid % grid/cardinal-directions (fn [ch] (not (= \. ch)))))
 
 (defn next-target [grid neighbors agents current-agent-id]
-    (let [current-agent (agents current-agent-id)
-          agent-coords (:coords current-agent)
-          agent-type (:type current-agent)
-          other-type (case agent-type \E \G \G \E)
-          distances (grid/dijkstra-search
-                     agent-coords
-                     neighbors
-                     (fn [& _] false))
-          target-coords (->> (vals agents)
-                       (filter #(= other-type (:type %)))
-                       (map :coords))
-          nearby-targets (reduce concat [] (map neighbors target-coords))
-          target (->> nearby-targets
-                      (filter distances)
-                      (sort (fn [c1 c2]
-                              (let [distance-comp (compare (distances c1) (distances c2))]
-                                (if
-                                 (= distance-comp 0) (reading-order-compare c1 c2)
-                                 distance-comp))))
-                      (first))]
-      [target (distances target)]))
+  (let [current-agent (agents current-agent-id)
+        agent-coords (:coords current-agent)
+        agent-type (:type current-agent)
+        other-type (case agent-type \E \G \G \E)
+        target-coords (->> (vals agents)
+                           (filter #(= other-type (:type %)))
+                           (map :coords))]
+    (if (empty? (filter (partial grid/is-adjacent? agent-coords) target-coords))
+      (let [distances (grid/dijkstra-search
+                       agent-coords
+                       neighbors
+                       (fn [& _] false))
+            nearby-targets (reduce concat [] (map neighbors target-coords))
+            target (->> nearby-targets
+                        (filter distances)
+                        (sort (fn [c1 c2]
+                                (let [distance-comp (compare (distances c1) (distances c2))]
+                                  (if
+                                   (= distance-comp 0) (reading-order-compare c1 c2)
+                                   distance-comp))))
+                        (first))]
+        [target (distances target)])
+      ;; don't move
+      [agent-coords 0]
+      )))
 
 (let [grid (grid/parse (utils/read-input "2018/day15-move-example.txt"))]
   (grid/breadth-first-search
@@ -60,38 +64,49 @@
 (let [grid (grid/parse (utils/read-input "2018/day15-move-example.txt"))]
   (grid/all-paths [1 1] [2 2] (neighbors grid) (fn [& args] false)))
 
-(defn agent-move [grid neighbors agents current-agent-id]
-  (let [_ (println current-agent-id)
+(defn agent-move [grid agents current-agent-id]
+  (let [neighbors (neighbors grid)
         current-square (get-in agents [current-agent-id :coords])
-        [target target-dist] (next-target grid neighbors agents current-agent-id)]
-    (assoc-in
-     agents
-     [current-agent-id :coords]
-     (if (= target current-square)
-       current-square
-       (->> (grid/all-paths (get-in agents [current-agent-id :coords]) target neighbors
-                            (fn [_ d] (> d target-dist)))
-            (sort-by first reading-order-compare)
+        [target target-dist] (next-target grid neighbors agents current-agent-id)
+        _ (println "current square" current-square "target is" target)]
+    (if (nil? target)
+      ;; no path to target, or all targets death.
+      agents
+      (assoc-in
+       agents
+       [current-agent-id :coords]
+       (if (= target current-square)
+         current-square
+         (do
+           (->> (grid/all-paths (get-in agents [current-agent-id :coords]) target neighbors
+                                (fn [_ d] (> d target-dist)))
+                (sort-by second reading-order-compare)
             ;; first path is the "reading order first"
-            (first)
+                (first)
             ;; second element of it is the next step
-            (second))))))
+                (second))))))))
 
 (defn agent-id-order [agents]
   (->> (vals agents)
        (sort-by :coords reading-order-compare)
        (map :id)))
 
-(defn tick [grid]
-  (let [neighbors (memoize (neighbors grid))]
-    (fn [agents]
-      (reduce
-       (partial agent-move grid neighbors)
-       agents
-       (agent-id-order agents)))))
+(defn tick [[grid agents]]
+  (reduce
+   (fn [[grid agents] current-agent-id]
+     (let [[ox oy] (get-in agents [current-agent-id :coords])
+           next-agents (agent-move grid agents current-agent-id)
+           [nx ny] (get-in next-agents [current-agent-id :coords])
+           _ (println "moving current-agent-id from" [ox oy] "to" [nx ny])]
+       [(-> grid
+            (assoc-in [oy ox] \.)
+            (assoc-in [ny nx] (get-in agents [current-agent-id :type])))
+        next-agents]))
+   [grid agents]
+   (agent-id-order agents)))
 
 (let [grid (grid/parse (utils/read-input "2018/day15-move-example2.txt"))]
-  (-> (iterate (tick grid) (agents grid))))
+  (nth (map first (iterate tick [grid (agents grid)])) 3))
 
 (let [grid (grid/parse (utils/read-input "2018/day15-move-example.txt"))
           agents (agents grid)
