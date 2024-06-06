9956be0d5cd85f72fc92a17379c11f5ea69fd90f
Dave King <tildedave@gmail.com>
2024-06-06 09:14:36 -0400

    [2018] Day 18 - second star

--- src/advent2018/day18.clj
+++ src/advent2018/day18.clj
@@ -59,35 +59,50 @@
         (for [x (range min-x (inc max-x))]
           (cond
             (contains? trees [x y]) \|
-          (contains? lumberyards [x y]) \#
+            (contains? lumberyards [x y]) \#
             :else \.)))))))
 
 (defn parse-grid [filename]
   (let [grid (grid/parse (utils/read-input filename))]
     [(as-> (grid/coords grid) s
-      (group-by (fn [coord] (case
-                             (grid/at grid coord)
-                              \. :open
-                              \# :lumberyards
-                              \| :trees)) s)
-      (dissoc s :open)
-      (update-vals s set))
-     (grid/bounds grid)]
-     ))
+       (group-by (fn [coord] (case
+                              (grid/at grid coord)
+                               \. :open
+                               \# :lumberyards
+                               \| :trees)) s)
+       (dissoc s :open)
+       (update-vals s set))
+     (grid/bounds grid)]))
+
+(defn resource-value [state]
+  (->> state (vals) (map count) (reduce *)))
 
 (defn answer-part1 [filename]
-  (let [[grid [xmax ymax]] (parse-grid filename)
-        _ (println xmax ymax)]
-    (reduce
-     *
-     (-> (iterate (partial tick (neighbors [0 (dec xmax)] [0 (dec ymax)])) grid)
-         (nth 10)
-         (update-vals count)
-         (vals)))))
+  (let [[grid [xmax ymax]] (parse-grid filename)]
+    (-> (iterate (partial tick (neighbors [0 (dec xmax)] [0 (dec ymax)])) grid)
+        (nth 10)
+        (resource-value))))
 
 (answer-part1 "2018/day18.txt")
 
-(-> (iterate (partial tick (neighbors [0 9] [0 9])) (parse-grid "2018/day18-example.txt"))
-    (nth 10)
-    (update-vals count)
-    (vals))
+(defn answer-part2 [filename]
+  (let [[grid [xmax ymax]] (parse-grid filename)
+        tick-seq (iterate (partial tick (neighbors [0 (dec xmax)] [0 (dec ymax)])) grid)
+        [loopstart loopend] (->> tick-seq
+                                 (map print-grid)
+                                 (map-indexed vector)
+                                 (take 10000)
+                                 (reduce (fn [seen [n val]]
+                                           (if (contains? seen val)
+                                             (reduced [(seen val) n])
+                                             (assoc seen val n)))
+                                 {}))]
+    (-> tick-seq
+        (nth (+ loopstart (mod (- 1000000000 loopstart) (- loopend loopstart))))
+        (resource-value))))
+
+(answer-part2 "2018/day18.txt")
+
+;; loop is at 513, goes to 541
+;; length 28
+
