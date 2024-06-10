c13b72f4b422fee0dbc66989866eb648bf7d7a51
Dave King <tildedave@gmail.com>
2024-06-09 21:12:42 -0400

    [2018] Day 23 - second star

--- src/advent2018/day23.clj
+++ src/advent2018/day23.clj
@@ -10,7 +10,7 @@
                        (map utils/parse-int))]
     {:coords [x y z] :radius r :id (random-uuid)}))
 
-(defn in-range? [nanobot1 nanobot2]
+(defn nanobot-in-range? [nanobot1 nanobot2]
   (<=
    (utils/manhattan-distance (:coords nanobot1) (:coords nanobot2))
    (:radius nanobot1)))
@@ -18,7 +18,7 @@
 (let [nanobots (map parse-nanobot (utils/read-input "2018/day23.txt"))
       max-bot (first (sort-by :radius > nanobots))]
   (->> nanobots
-       (filter (partial in-range? max-bot))
+       (filter (partial nanobot-in-range? max-bot))
        (count)))
 
 ;; so it seems like the approach we want is see which nanobots intersect which
@@ -68,37 +68,74 @@
 ;; compute this, and we can compute their intersections.
 ;; we are going to be kind of stupider and try a gradient descent approach.
 
-(defn total-distance-to-all [[x y z] nanobots]
-  (->> nanobots
-       (map #(utils/manhattan-distance [x y z] (:coords %)))
-       (reduce +)))
+(defn in-range? [[x y z] nanobot]
+  (<=
+   (utils/manhattan-distance (:coords nanobot) [x y z])
+   (:radius nanobot)))
 
-(/ 5.5 2)
+(defn gradient-descent [nanobots [[x0 x1] [y0 y1] [z0 z1] step-size]]
+  (if (zero? step-size)
+    [[x0 x1] [y0 y1] [z0 z1] step-size]
+    (let [[bx by bz]
+          (->>
+           (for [x (range x0 (inc x1) step-size)
+                 y (range y0 (inc y1) step-size)
+                 z (range z0 (inc z1) step-size)]
+             [x y z])
+           (map #(vector % (count (filter (partial in-range? %) nanobots))))
+           (sort (fn [[coords1 n1] [coords2 n2]]
+                      (if (not= n1 n2)
+                        (compare n2 n1)
+                        (compare (utils/manhattan-distance coords1 [0 0 0])
+                                 (utils/manhattan-distance coords2 [0 0 0])))))
+           (first)
+           (first))]
+      (if (= step-size 1)
+        [[bx bx] [by by] [bz bz] 0]
+        [[(- bx step-size) (+ bx step-size)]
+         [(- by step-size) (+ by step-size)]
+         [(- bz step-size) (+ bz step-size)]
+         (quot step-size 2)]))))
 
-(defn gradient-descent [nanobots [x y z]]
-  (let [dist (total-distance-to-all [x y z] nanobots)
-        ;; we can probably step further at once, but let's start here.
-        step-length 1] ;; (int (/ dist 4))]
-    (->>
-     (for [dx (range -1 2)
-           dy (range -1 2)
-           dz (range -1 2)]
-       (if (= dx dy dz 0)
-         nil
-         [(+ x (* dx step-length))
-          (+ y (* dy step-length))
-          (+ z (* dz step-length))]))
-     (remove nil?)
-     ;; TODO: break ties by closeness to origin???
-     (sort-by #(total-distance-to-all % nanobots))
-     (first))))
+;; (gradient-descent)
+;;   (let [dist (total-distance-to-all [x y z] nanobots)
+;;         ;; we can probably step further at once, but let's start here.
+;;         step-length 1] ;; (int (/ dist 4))]
+;;     (->>
+;;      (for [dx (range -1 2)
+;;            dy (range -1 2)
+;;            dz (range -1 2)]
+;;        (if (= dx dy dz 0)
+;;          nil
+;;          [(+ x (* dx step-length))
+;;           (+ y (* dy step-length))
+;;           (+ z (* dz step-length))]))
+;;      (remove nil?)
+;;      ;; TODO: break ties by closeness to origin???
+;;      (sort-by #(total-distance-to-all % nanobots))
+;;      (first))))
 
-(let [[start-coords nanobots] (gradient-descent-start "2018/day23-example.txt")]
+(* 2 2 2 2 2 2 2 2)
+
+;; part 2 answer based on https://github.com/mfs/aoc-2018/blob/master/src/bin/p23.rs
+;; my brain is just mush w/computational geometry
+(let [[start-coords nanobots] (gradient-descent-start "2018/day23.txt")
+      xs (map #(get-in % [:coords 0]) nanobots)
+      [x0 x1] [(reduce min xs) (reduce max xs)]
+      ys (map #(get-in % [:coords 1]) nanobots)
+      [y0 y1] [(reduce min ys) (reduce max ys)]
+      zs (map #(get-in % [:coords 2]) nanobots)
+      [z0 z1] [(reduce min zs) (reduce max zs)]
+      step-size (loop [step-size 1]
+                  (if (>= step-size (- x1 x0))
+                    step-size
+                    (recur (* step-size 2))))
+      _ (println step-size)
+      _ (println x0 z1 y0 y1 z0 z1)]
   ;; (total-distance-to-all [0 0 0] nanobots))
-  (iterate (partial gradient-descent nanobots) start-coords))
-
-
-
-;; (abs (- r1 r2)) manhattan distance c1 c2 (abs (+ r1 r2))
-
-;; the intersection of two nanobots is not a circle.
+  (->> (iterate (partial gradient-descent nanobots) [[x0 x1] [y0 y1] [z0 z1] step-size])
+       (drop-while #(not (zero? (last %))))
+       (first)
+       (take 3)
+       (map first)
+       (reduce +)))
