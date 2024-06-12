a12fbb100bbd4c10ad781e5fe91f7811eb655669
Dave King <tildedave@gmail.com>
2024-06-12 17:31:36 -0400

    [2019] Day 3 - second star

--- src/advent2019/day3.clj
+++ src/advent2019/day3.clj
@@ -13,7 +13,7 @@
                  \L :left)
                (utils/parse-int (.substring s 1))]))))
 
-(defn step [{:keys [coords seen]}
+(defn step [{:keys [coords seen total-steps]}
             [direction num-steps]]
   (let [[x y] coords
         [dx dy] (case direction
@@ -22,32 +22,50 @@
                   :up [0 -1]
                   :down [0 1])]
        {:coords [(+ x (* num-steps dx)) (+ y (* num-steps dy))]
+        :total-steps (+ num-steps total-steps)
         :seen
-        (merge-with
-        +
+        (merge
         seen
         (->> (range 1 (inc num-steps))
-             (map (fn [n] [(* n dx) (* n dy)]))
-             (map (fn [[dx dy]] {[(+ x dx) (+ y dy)] 1}))
+             (map (fn [n] [(+ n total-steps) (* n dx) (* n dy)]))
+             (map (fn [[n dx dy]] {[(+ x dx) (+ y dy)] n}))
              (reduce merge {})))}))
 
-(defn answer [wire1 wire2]
-  (let [coord-set (fn [wire] (->> wire
+(defn answer-part1 [wire1 wire2]
+  (let [seen-coords (fn [wire] (->> wire
                                     (parse-commands)
-                                    (reduce step {:coords [0 0] :seen {}})
-                                    :seen
-                                    (keys)
-                                    (set)))]
+                                    (reduce step {:coords [0 0] :seen {} :total-steps 0})
+                                    :seen))
+        coord-set (fn [wire] (->> (seen-coords wire) (keys) (set)))]
     (->> (set/intersection (coord-set wire1) (coord-set wire2))
          (sort-by (partial utils/manhattan-distance [0 0]))
          (first)
          (utils/manhattan-distance [0 0]))))
 
-(answer "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+(answer-part1 "R75,D30,R83,U83,L12,D49,R71,U7,L72"
         "U62,R66,U55,R34,D71,R55,D58,R83")
 
-(answer "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+(answer-part1 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
         "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
 
 ;; part 1
-(apply answer (utils/read-input "2019/day3.txt"))
+(apply answer-part1 (utils/read-input "2019/day3.txt"))
+
+(defn answer-part2 [wire1 wire2]
+  (let [seen-coords (fn [wire] (->> wire
+                                    (parse-commands)
+                                    (reduce step {:coords [0 0] :seen {} :total-steps 0})
+                                    :seen))
+        seen-wire1 (seen-coords wire1)
+        seen-wire2 (seen-coords wire2)]
+    (->> (set/intersection (set (keys seen-wire1)) (set (keys seen-wire2)))
+         (map (fn [coord] (+ (seen-wire1 coord)
+                             (seen-wire2 coord))))
+         (sort)
+         (first))))
+
+(answer-part2 "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+              "U62,R66,U55,R34,D71,R55,D58,R83")
+
+;; part 2
+(apply answer-part2 (utils/read-input "2019/day3.txt"))
