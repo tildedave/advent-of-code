9fdfb9d0ef2a44600a1bb416d6d7aab455b91afb
Dave King <tildedave@gmail.com>
2024-06-15 07:28:35 -0400

    [2019] Day 6 - no topo sort, just memoized parent

--- src/advent2019/day6.clj
+++ src/advent2019/day6.clj
@@ -1,6 +1,7 @@
 (ns advent2019.day6
   (:require [grid :as grid]
-            [utils :as utils]))
+            [utils :as utils]
+            [clojure.set :as set]))
 
 ;; I suppose this is easiest to do using DFS.
 
@@ -12,21 +13,50 @@
 
 (parse-orbits (utils/read-input "2019/day6-example.txt"))
 
+;; memoized recursive function
+(defn num-orbits [parents]
+  (let [num-orders-rec (fn [rec n]
+                            (if-let [parent (parents n)]
+                              (inc (rec rec parent))
+                              0))
+        memoized-rec (memoize num-orders-rec)]
+    (partial memoized-rec memoized-rec)))
+
+;; this is obviously a bit of a sledgehammer.
+;; we could topo sort this to get the answer quicker.
+;; yes we do need to topo sort.
 (defn answer-part1 [lines]
-  (let [orbits (parse-orbits lines)]
+  (let [orbits (parse-orbits lines)
+        num-orbits (num-orbits orbits)]
     (->> (keys orbits)
-         (map #(-> (grid/breadth-first-search
-                    %
-                    (fn [n]
-                      (if (contains? orbits n)
-                        [(orbits n)]
-                        [])))
-                   (first)
-                   (keys)
-                   (set)
-                   (disj %)))
-         (map count)
-         (reduce +))))
+         (map num-orbits)
+         (reduce + 0))))
 
 (answer-part1 (utils/read-input "2019/day6-example.txt"))
 (answer-part1 (utils/read-input "2019/day6.txt"))
+
+(defn all-orbits [parents]
+   (let [all-orbits-rec (fn [rec n]
+                          (if-let [parent (parents n)]
+                            (conj (rec rec parent) n)
+                            [n]))
+         memoized-rec (memoize all-orbits-rec)]
+     (partial memoized-rec memoized-rec)))
+
+((all-orbits (parse-orbits (utils/read-input "2019/day6-example2.txt"))) "YOU")
+((all-orbits (parse-orbits (utils/read-input "2019/day6-example2.txt"))) "SAN")
+
+(defn answer-part2 [lines]
+  (let [orbits (parse-orbits lines)
+        all-orbits (all-orbits orbits)]
+    (loop [orbits-you (all-orbits "YOU")
+           orbits-san (all-orbits "SAN")]
+      (if (= (first orbits-you)
+             (first orbits-san))
+        (recur (subvec orbits-you 1)
+               (subvec orbits-san 1))
+        (+ (count orbits-you) (count orbits-san) -2)))))
+
+(answer-part2 (utils/read-input "2019/day6-example2.txt"))
+(answer-part2 (utils/read-input "2019/day6.txt"))
+
