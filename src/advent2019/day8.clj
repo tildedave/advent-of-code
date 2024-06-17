fd6147f1beaa4c0ad81d7861a0e54bfd1520ed36
Dave King <tildedave@gmail.com>
2024-06-17 13:00:14 -0400

    [2019] Day 8 - second star

--- src/advent2019/day8.clj
+++ src/advent2019/day8.clj
@@ -1,5 +1,6 @@
 (ns advent2019.day8
-  (:require [utils :as utils]))
+  (:require [utils :as utils]
+            [clojure.string :as string]))
 
 (defn layers [w h seq]
   (partition h (partition w seq)))
@@ -13,6 +14,7 @@
   (let [res (group-by identity (flatten layer))]
     (* (count (res 1)) (count (res 2)))))
 
+
 ;; part 1 answer
 (->> (utils/read-input "2019/day8.txt")
      (first)
@@ -22,3 +24,28 @@
      (sort-by num-ones)
      (first)
      (answer-part1))
+
+(defn pixel-result
+  "p1 is in front of p2"
+  [p1 p2]
+  (case p1
+    2 p2
+    1 1
+    0 0))
+
+;; part 2
+(->> (utils/read-input "2019/day8.txt")
+     (first)
+     (seq)
+     (map #(utils/parse-int (str %)))
+     (layers 25 6)
+     ;; no need to supply an initial
+     (reduce
+      (fn [layer current-layer]
+        (map (fn [row current-row]
+               (map pixel-result row current-row))
+             layer current-layer)))
+     (map #(map (fn [n] (case n 1 \# \.)) %))
+     (map string/join)
+     (string/join "\n")
+     (println))
