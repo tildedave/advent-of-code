d0a22f85f876960be4a223d808b078d1af12ad28
Dave King <tildedave@gmail.com>
2024-06-27 12:58:45 -0400

    [2019] Day 16 - second star

--- src/advent2019/day16.clj
+++ src/advent2019/day16.clj
@@ -10,7 +10,7 @@
                   (cycle base-pattern))))))
 
 (defn parse-input [s]
-  (map #(utils/parse-int (str %)) (seq s)))
+  (mapv #(utils/parse-int (str %)) (seq s)))
 
 (defn apply-phase [input-signal]
   (->> (range 1 (inc (count input-signal)))
@@ -34,4 +34,62 @@
 (answer-part1 "69317163492948606335995924319873")
 (answer-part1 (utils/read-input-line "2019/day16.txt"))
 
+(as-> (parse-input "80871224585914546619083218645595") a
+      (iterate apply-phase a))
+
 ;; so part 2 needs to be sneakier
+;; for part 2, as long as the offset is > halfway point,
+;; the pattern is all 1s, so we can compute the digits on
+;; the answer directly.
+;; I suppose dynamic programming is the simplest
+;; I wish there were a more elegant way to do this in Clojure.
+;; I could of course just re-implement my golang logic, but
+;; that seems against the spirit of Clojure :-)
+
+;; the way I did it before: compute the digits downwards.
+;; I guess I also didn't compute the final digit since it's always the
+;; same, but whatever.
+
+(defn initial-digits [input-signal]
+  (repeat 100 (last input-signal)))
+
+
+(count (parse-input "80871224585914546619083218645595"))
+(mod (dec (* 32 10000)) 32)
+
+(defn digits [input-signal prev-digits idx]
+  (loop [last-digit (nth input-signal (mod idx (count input-signal)))
+         prev-digits prev-digits
+         result []]
+    (if-let [x (first prev-digits)]
+      (let [next-digit (mod (+ last-digit x) 10)]
+        (recur
+         next-digit
+         (rest prev-digits)
+         (conj result next-digit)))
+      result)))
+
+(utils/parse-int (.substring "03036732577212944063491565474664" 0 7))
+
+(defn answer-part2 [line]
+  (let [is (parse-input line)
+        offset (utils/parse-int (.substring line 0 7))]
+    (->>
+     [(initial-digits is) (dec (* (count is) 10000))]
+     (iterate
+      (fn [[prev-digits idx]]
+        [(digits is prev-digits idx) (dec idx)]))
+     ;; I'm not certain why we need to tweak everything by 1,
+     ;; I'd guess my indices are off by one.
+     (drop-while (fn [[_ idx]] (not (<= (dec offset) idx (+ offset 6)))))
+     (take 8)
+     (map first)
+     (map last)
+     (reverse)
+     (string/join)
+     )))
+
+(answer-part2 "03036732577212944063491565474664")
+(answer-part2 "02935109699940807407585447034323")
+(answer-part2 "03081770884921959731165446850517")
+(answer-part2 (utils/read-input-line "2019/day16.txt"))
