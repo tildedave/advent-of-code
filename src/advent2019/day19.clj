76f41d8e980bab4cbaa39077a077448544477050
Dave King <tildedave@gmail.com>
2024-06-29 14:18:08 -0400

    [2019] Day 19 - second star (from reddit)

--- src/advent2019/day19.clj
+++ src/advent2019/day19.clj
@@ -12,14 +12,22 @@
       (<! output))))
 
 ;; part 1
- (println
-  (let [program (intcode/parse-file "2019/day19.txt")]
-  (->>
-   (for [x (range 50)
-        y (range 50)]
-    (<!! (test-square program x y)))
-   (filter (partial = 1))
-   (count))))
+(println
+ (let [program (intcode/parse-file "2019/day19.txt")]
+   (->>
+    (for [x (range 50)
+          y (range 50)]
+      (<!! (test-square program x y)))
+    (filter (partial = 1))
+    (count))))
 
-
-12
+;; so I made part 2 very complicated in golang.
+;; this problem is brute forceable.  we will just do this.
+;; from https://www.reddit.com/r/adventofcode/comments/ecogl3/comment/fbcxvt1/
+(let [program (intcode/parse-file "2019/day19.txt")]
+  (loop [x 0 y 100]
+    (if (= (<!! (test-square program x y)) 1)
+      (if (= (<!! (test-square program (+ x 99) (- y 99))) 1)
+        (+ (* x 10000) (- y 99))
+        (recur x (inc y)))
+      (recur (inc x) y))))
