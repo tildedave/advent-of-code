9ccb189aacc8adeeb1636838b92713e937015ed0
Dave King <tildedave@gmail.com>
2024-08-16 09:09:31 -0400

    [2023] Day 21 - second star

--- src/advent2023/day21.clj
+++ src/advent2023/day21.clj
@@ -23,33 +23,53 @@
 (let [grid (grid/parse example-grid)]
   (grid/neighbors grid (starting-coords grid) grid/cardinal-directions (fn [ch] (not= ch \.))))
 
-(defn neighbors [grid coord]
-  (grid/neighbors grid coord grid/cardinal-directions
-  (fn [ch] (= ch \#))))
+(grid/bounds (grid/parse example-grid))
+
+(defn neighbors [[xmax ymax] grid [x y]]
+  (->>
+   (for [[dx dy] grid/cardinal-directions]
+    (let [[nx ny] [(+ x dx) (+ y dy)]]
+      (case (grid/at grid [(mod nx xmax) (mod ny ymax)])
+        \# nil
+        [nx ny])))
+   (remove nil?)))
 
 (defn reachable-positions [grid num-steps]
-  (loop
-   [queue [[(starting-coords grid) num-steps]]
-    seen #{}
-    result #{}]
+  (let [neighbors (partial neighbors (grid/bounds grid))]
+    (loop
+     [queue [[(starting-coords grid) num-steps]]
+      seen #{}
+      result #{}]
     ;; (println queue seen result)
-    (if (empty? queue)
-      result
-      (let [[coord steps-left] (first queue)
-            queue (subvec queue 1)
-            seen (conj seen [coord steps-left])]
-        (if (= steps-left 0)
-          (recur queue seen (conj result coord))
-          (let [[queue seen]
-                (reduce
-                 (fn [[queue seen] coord]
-                   (if (contains? seen [coord (dec steps-left)])
-                     [queue seen]
-                     [(conj queue [coord (dec steps-left)])
-                      (conj seen [coord (dec steps-left)])]))
-                 [queue seen]
-                 (neighbors grid coord))]
-            (recur queue seen result)))))))
+      (if (empty? queue)
+        result
+        (let [[coord steps-left] (first queue)
+              queue (subvec queue 1)
+              seen (conj seen [coord steps-left])]
+          (if (= steps-left 0)
+            (recur queue seen (conj result coord))
+            (let [[queue seen]
+                  (reduce
+                   (fn [[queue seen] coord]
+                     (if (contains? seen [coord (dec steps-left)])
+                       [queue seen]
+                       [(conj queue [coord (dec steps-left)])
+                        (conj seen [coord (dec steps-left)])]))
+                   [queue seen]
+                   (neighbors grid coord))]
+              (recur queue seen result))))))))
 
 (count (reachable-positions (grid/parse example-grid) 6))
-(println (count (reachable-positions (grid/parse-file "2023/day21.txt") 64)))
+(count (reachable-positions (grid/parse-file "2023/day21.txt") 458))
+(+ 327 131)
+(let [grid (grid/parse-file "2023/day21.txt")]
+  (map
+   #(count (reachable-positions grid %))
+   '(65 196 327)))
+
+
+;; solved via quadratic interpolation https://www.johndcook.com/quadratic_interpolator.html
+;; 0 -> 3943
+;; 1 -> 35126
+;; 2 -> 97407
+;; answer is at x = (26501365 - 65 ;first square) / 131 ; square length = 202300
