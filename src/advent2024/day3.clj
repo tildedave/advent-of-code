2e417113bc8291e3416a76b39fda65ec115f88d9
Dave King <tildedave@gmail.com>
2024-12-03 05:41:23 -0500

    [2024] Day 3 - second star

--- src/advent2024/day3.clj
+++ src/advent2024/day3.clj
@@ -12,3 +12,26 @@
 (->> (utils/read-input "2024/day3.txt")
      (map total-line)
      (reduce +))
+
+(defn total-line-part2
+  ([line] (total-line-part2 [:do 0] line))
+  ([start line]
+   (->> line
+        (re-seq #"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+        (reduce
+         (fn [[status acc] [directive & args]]
+           (case directive
+             "don't()" [:dont acc]
+             "do()" [:do acc]
+             (case status
+               :do [status (+ acc (reduce * (map utils/parse-int args)))]
+               :dont [status acc])))
+         start))))
+
+(def example-line-part2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+(total-line-part2 example-line-part2)
+
+(->> (utils/read-input "2024/day3.txt")
+     (reduce total-line-part2 [:do 0])
+     (second))
