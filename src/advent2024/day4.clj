111aeb9beacbf1f61a69965b2d5e67b90047f318
Dave King <tildedave@gmail.com>
2024-12-04 06:53:17 -0500

    [2024] Day 4 - second star

--- src/advent2024/day4.clj
+++ src/advent2024/day4.clj
@@ -44,5 +44,31 @@
    (filter #(= % '(\X \M \A \S)))
    (count))))
 
+(defn is-x? [grid coords]
+  ;; we're an A at our coord
+  ;; in one direction, two are Ms, the rest of Ss
+  ;; we can't have 1 1 | -1 1 both Ms
+  (let [neighbors (map #(vector % (mapv + coords %)) '([-1 -1] [-1 1] [1 -1] [1 1]))
+        neighbor-coords (map second neighbors)]
+    (cond
+      (not= (grid/at grid coords) \A) false
+      (seq (filter (partial grid/out-of-bounds? grid) neighbor-coords)) false
+      :else
+      (contains?
+       #{{[-1 -1] \M [1 -1] \M [-1 1] \S [1 1] \S}
+         {[-1 -1] \S [1 -1] \S [-1 1] \M [1 1] \M}
+         {[-1 -1] \M [1 -1] \S [-1 1] \M [1 1] \S}
+         {[-1 -1] \S [1 -1] \M [-1 1] \S [1 1] \M}}
+       (reduce into {} (map (fn [[d coords]] {d (grid/at grid coords)}) neighbors))))))
+
+(defn answer-part2 [lines]
+  (let [grid (grid/parse lines)]
+    (->>
+     (grid/coords grid)
+     (filter (partial is-x? grid))
+     (count))))
+
 (answer-part1 example-lines)
+(answer-part2 example-lines)
 (answer-part1 (utils/read-input "2024/day4.txt"))
+(answer-part2 (utils/read-input "2024/day4.txt"))
