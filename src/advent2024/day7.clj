199ea845d563d662de0984bfdd1c99062fcef81e
Dave King <tildedave@gmail.com>
2024-12-07 08:18:50 -0500

    [2024] Day 7 - second star

--- src/advent2024/day7.clj
+++ src/advent2024/day7.clj
@@ -11,41 +11,80 @@
       (zero? (mod num final)) (recur (subvec num-list 0 (dec l)) (quot num final))
       :else (recur (subvec num-list 0 (dec l)) (- num final)))))
 
+(defn ends-with-str? [num suffix]
+  (.endsWith (str num) (str suffix)))
+
+(defn strip-num-suffix [num suffix]
+  (let [result (parse-long (.substring (str num) 0 (.lastIndexOf (str num) (str suffix))))]
+    (if (nil? result) 0 result)))
+
+(strip-num-suffix 4555 5)
+
 (defn can-reach-brute-force? [num-list num]
   (let [l (count num-list)
         final (nth num-list (dec l))]
     (cond
-     (= l 1) (= (first num-list) num)
-     (zero? (mod num final))
-     (or
-      (can-reach-brute-force? (subvec num-list 0 (dec l)) (quot num final))
-      (can-reach-brute-force? (subvec num-list 0 (dec l)) (- num final)))
-     :else
-     (recur (subvec num-list 0 (dec l)) (- num final)))))
+      (= l 1) (= (first num-list) num)
+      (zero? (mod num final))
+      (or
+       (can-reach-brute-force? (subvec num-list 0 (dec l)) (quot num final))
+       (can-reach-brute-force? (subvec num-list 0 (dec l)) (- num final)))
+      :else
+      (recur (subvec num-list 0 (dec l)) (- num final)))))
 
-     (defn parse-line [s]
-       (->> (re-matches #"(\d+):((?: \d+)+)" s)
-            (rest)
-            ((fn [[a r]] [(parse-long a) (vec (utils/parse-number-list (.trim r)))]))))
+(defn can-reach-brute-force-part2? [num-list num]
+  (let [l (count num-list)
+        final (nth num-list (dec l))]
+    (cond
+      (< num 0) false
+      (= l 1) (= (first num-list) num)
+      :else (let [multiply?
+            (if (zero? (mod num final))
+              (can-reach-brute-force-part2? (subvec num-list 0 (dec l)) (quot num final))
+              false)
+            concat?
+            (if (ends-with-str? num final)
+              (can-reach-brute-force-part2? (subvec num-list 0 (dec l)) (strip-num-suffix num final))
+              false)]
+        (or multiply? concat? (recur (subvec num-list 0 (dec l)) (- num final)))))))
 
-     (parse-line "190: 10 19")
+(defn parse-line [s]
+  (->> (re-matches #"(\d+):((?: \d+)+)" s)
+       (rest)
+       ((fn [[a r]] [(parse-long a) (vec (utils/parse-number-list (.trim r)))]))))
 
-     (def example-lines '("190: 10 19"
-                          "3267: 81 40 27"
-                          "83: 17 5"
-                          "156: 15 6"
-                          "7290: 6 8 6 15"
-                          "161011: 16 10 13"
-                          "192: 17 8 14"
-                          "21037: 9 7 18 13"
-                          "292: 11 6 16 20"))
+(parse-line "190: 10 19")
 
-     (->> (map parse-line example-lines)
-          (filter (fn [[num num-list]] (can-reach? num-list num)))
-          (map first)
-          (reduce +))
+(def example-lines '("190: 10 19"
+                     "3267: 81 40 27"
+                     "83: 17 5"
+                     "156: 15 6"
+                     "7290: 6 8 6 15"
+                     "161011: 16 10 13"
+                     "192: 17 8 14"
+                     "21037: 9 7 18 13"
+                     "292: 11 6 16 20"))
 
-     (->> (map parse-line (utils/read-input "2024/day7.txt"))
-          (filter (fn [[num num-list]] (can-reach? num-list num)))
-          (map first)
-          (reduce +))
+(can-reach-brute-force-part2? [15 6] 156)
+(can-reach-brute-force-part2? [6 8 6 15] 7290)
+
+(/ 7290 15)
+(->> (map parse-line example-lines)
+     (filter (fn [[num num-list]] (can-reach-brute-force? num-list num)))
+     (map first)
+     (reduce +))
+
+(->> (map parse-line example-lines)
+     (filter (fn [[num num-list]] (can-reach-brute-force-part2? num-list num)))
+     (map first)
+     (reduce +))
+
+(->> (map parse-line (utils/read-input "2024/day7.txt"))
+     (filter (fn [[num num-list]] (can-reach-brute-force? num-list num)))
+     (map first)
+     (reduce +))
+
+(->> (map parse-line (utils/read-input "2024/day7.txt"))
+     (filter (fn [[num num-list]] (can-reach-brute-force-part2? num-list num)))
+     (map first)
+     (reduce +))
