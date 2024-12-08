030cdc45fc6f0530c767f9e6bbc4f019b4034ad0
Dave King <tildedave@gmail.com>
2024-12-08 06:47:51 -0500

    [2024] Day 8 - second star \o/

--- src/advent2024/day8.clj
+++ src/advent2024/day8.clj
@@ -1,7 +1,8 @@
 (ns advent2024.day8
   (:require
    [grid :as grid]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [utils :as utils]))
 
 (def example-lines
   '("............"
@@ -33,18 +34,71 @@
          y coord-list]
      (if (= x y)
        nil
-       (mapv - x (mapv - x y))))
+       (mapv - y (mapv - x y))))
    (remove nil?)
    (remove (partial grid/out-of-bounds? grid))
    (set)))
 
-(let [grid (grid/parse example-lines)]
-  (as-> (antennae-coords grid) v
-    (update-vals v (partial antinodes grid))
-    (vals v)
-    (reduce set/union v)))
+(defn total-antinodes-part1 [lines]
+  (let [grid (grid/parse lines)]
+    (as-> (antennae-coords grid) v
+      (update-vals v (partial antinodes grid))
+      (vals v)
+      (reduce set/union v)
+      (count v))))
+
+(total-antinodes-part1 (utils/read-input "2024/day8.txt"))
 
 (let [grid (grid/parse example-lines)]
   (antinodes
    grid
    ((antennae-coords grid) \A)))
+
+(def second-example
+  '("T....#...."
+    "...T......"
+    ".T....#..."
+    ".........#"
+    "..#......."
+    ".........."
+    "...#......"
+    ".........."
+    "....#....."
+    ".........."))
+
+(defn antinodes-part2 [grid coord-list]
+  (->>
+   (for [x coord-list
+         y coord-list]
+     (let [offset (mapv - x y)]
+       (if (= x y)
+         #{y}
+         (loop
+          [curr y
+           result #{}]
+           (if (grid/out-of-bounds? grid curr)
+             result
+             (recur (mapv - curr offset) (conj result curr)))))))
+   (reduce set/union)
+   ))
+
+(let [grid (grid/parse second-example)]
+  (antinodes-part2
+   grid
+   ((antennae-coords grid) \T)))
+
+(let [grid (grid/parse example-lines)]
+  (antinodes-part2
+   grid
+   ((antennae-coords grid) \0)))
+
+(defn total-antinodes-part2 [lines]
+  (let [grid (grid/parse lines)]
+    (as-> (antennae-coords grid) v
+      (update-vals v (partial antinodes-part2 grid))
+      (vals v)
+      (reduce set/union v)
+      (count v))))
+
+(total-antinodes-part2 example-lines)
+(total-antinodes-part2 (utils/read-input "2024/day8.txt"))
