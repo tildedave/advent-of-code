75e7fcec7891f9f6e5f4da80ca91ebd0ee877b6f
Dave King <tildedave@gmail.com>
2024-12-11 08:36:28 -0500

    [2024] Day 11 - part 2 - ez

--- src/advent2024/day11.clj
+++ src/advent2024/day11.clj
@@ -29,13 +29,39 @@
 
 (count (first (drop 25 (iterate blink [125 17]))))
 
+(* 49 2024)
 (blink [0 1 10 99 999])
-(->>  [0 1 10 99 999]
-;;  (
-    ;;   utils/read-input "2024/day11.txt")
-    ;;  (first)
-    ;;  (utils/parse-number-list)
-    ;;  (vec)
+(->> (utils/read-input "2024/day11.txt")
+     (first)
+     (utils/parse-number-list)
+     (vec)
      (iterate blink)
-     (drop 25)
-     (first))
+     (drop 35)
+     (first)
+     (count))
+
+(update {1 12} 1 (fnil + 0) 10)
+
+(defn blink-pt2 [stone-count]
+  (reduce
+   (fn [acc [num count]]
+     (cond
+       (zero? num) (update acc 1 (fnil + 0) count)
+       (has-even-digits? num) (let [[lower upper] (split-digits num)]
+                                (-> acc
+                                    (update lower (fnil + 0) count)
+                                    (update upper (fnil + 0) count)))
+       :else (update acc (* num 2024) (fnil + 0) count)))
+   {}
+   stone-count))
+
+
+(->> (utils/read-input "2024/day11.txt")
+     (first)
+     (utils/parse-number-list)
+     (frequencies)
+     (iterate blink-pt2)
+     (drop 75)
+     (first)
+     (vals)
+     (reduce +))
