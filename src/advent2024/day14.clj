70b95a4b76756ba00acd3134b8bbd1581a4f784e
Dave King <tildedave@gmail.com>
2024-12-14 08:48:51 -0500

    [2024] Day 14 - second star

--- src/advent2024/day14.clj
+++ src/advent2024/day14.clj
@@ -17,11 +17,31 @@
     "p=2,4 v=2,-3"
     "p=9,5 v=-3,-3"))
 
-(defn robot-move [xmax ymax [px py vx vy]]
-  [(mod (+ px vx) xmax)
-   (mod (+ py vy) ymax)
-   vx
-   vy])
+(defn display-grid [xmax ymax]
+  (vec
+   (for [y (range 0 ymax)]
+    (vec
+     (for [x (range 0 xmax)]
+      \.)))))
+
+(display-grid 11 7)
+
+(defn robot-move [xmax ymax [grid robots] [px py vx vy]]
+  (let [[nx ny] [(mod (+ px vx) xmax)
+                 (mod (+ py vy) ymax)]]
+    [(-> grid
+         (update-in [py px]
+                    (fn [ch]
+                      (case ch
+                        \. \.
+                        1 \.
+                        (dec ch))))
+         (update-in [ny nx]
+                    (fn [ch]
+                      (case ch
+                        \. 1
+                        (inc ch)))))
+     (conj robots [nx ny vx vy])]))
 
 (defn quadrant [xmax ymax [px py _ _]]
   (let [[mx my] [(/ (dec xmax) 2) (/ (dec ymax) 2)]]
@@ -35,9 +55,10 @@
 
 (as-> example-robots v
   (map #(vec (utils/str->nums %)) v)
-  (iterate #(map (partial robot-move 11 7) %) v)
-  (drop 100 v)
+  (iterate (fn [[g r]] (reduce (partial robot-move 11 7) [g []] r)) [(display-grid 11 7) v])
+  (drop 77 v)
   (first v)
+  (second v)
   (group-by (partial quadrant 11 7) v)
   (dissoc v -1)
   (vals v)
@@ -48,42 +69,70 @@
   (map #(vec (utils/str->nums %)) lines))
 
 (as-> (parse-robots (utils/read-input "2024/day14.txt")) v
-  (iterate #(map (partial robot-move 101 103) %) v)
+  (iterate (fn [[g r]] (reduce (partial robot-move 101 103) [g []] r)) [(display-grid 101 103) v])
   (drop 100 v)
   (first v)
+  (second v)
   (group-by (partial quadrant 101 103) v)
   (dissoc v -1)
   (vals v)
   (map count v)
   (reduce * v))
 
+;; distance from each other seems a bit expensive to compute
+;; could try to optimize some global metric, e.g. average distance from the center
+;; could try to see when each robot is closest to the center - that's a closed
+;; form calculation we can do
+
+(defn to-string [grid]
+  (string/join "\n" (map string/join grid)))
+
+(as-> (parse-robots (utils/read-input "2024/day14.txt")) v
+  (iterate (fn [[g r]] (reduce (partial robot-move 101 103) [g []] r)) [(display-grid 101 103) v])
+  (rest v)
+  (map first v)
+  (map to-string v)
+  (drop 1000 v)
+  (first v)
+  (println v))
+
 (defn robot-positions [robots]
   (->> robots (map (partial take 2)) (frequencies)))
 
 ;; we'll assume in the christmas tree every robot is next to every other one
 (defn adjacent-robot-count [robots]
-  ;; we can just do this dumbly, no need to handle wraps
   (let [robot-positions (robot-positions robots)]
-    (->> robots
-         (remove (fn [[x y]]
-                    (->>
-                     (for [[dx dy] [[-1 0] [1 0] [0 1] [0 -1]]]
-                       [dx dy])
-                     (map #(mapv + % [x y]))
-                     (filter #(contains? robot-positions %))
-                     (first)
-                     (nil?))))
-         (count))))
+    (->> robot-positions
+         (map (fn [[[x y] c]]
+                (if (nil?
+                     (->>
+                      (for [[dx dy] [[-1 0] [1 0] [0 1] [0 -1]]]
+                        [dx dy])
+                      (map #(mapv + % [x y]))
+                      (filter #(contains? robot-positions %))
+                      (first)))
+                  0
+                  c)))
+         (reduce +))))
 
-(adjacent-robot-count (parse-robots example-robots))
+;; woot
+;; (time
+;;  (as-> (parse-robots (utils/read-input "2024/day14.txt")) v
+;;    (iterate (fn [[g r]] (reduce (partial robot-move 101 103) [g []] r)) [(display-grid 101 103) v])
+;;    (map-indexed vector v)
+;;    (map (fn [[n [_ robots]]] [n (adjacent-robot-count robots)]) v)
+;;    (take (* 103 101) v)
+;;    (sort-by second > v)
+;;    (take 3 v)))
 
 
-(time (as-> (parse-robots (utils/read-input "2024/day14.txt")) v
-  (iterate #(map (partial robot-move 101 103) %) v)
-  (map-indexed vector v)
-  (map (fn [[n r]] [n (adjacent-robot-count r)]) v)
-  (drop 1000 v)
-        (first v)))
+(as-> (parse-robots (utils/read-input "2024/day14.txt")) v
+  (iterate (fn [[g r]] (reduce (partial robot-move 101 103) [g []] r)) [(display-grid 101 103) v])
+  (drop 8168 v)
+  (first v)
+  (first v)
+  (to-string v)
+  (println v))
 
 (defn robots-to-string [xmax ymax robots]
   (let [robot-positions (robot-positions robots)]
@@ -95,5 +144,5 @@
           (robot-positions [x y])
           \.)))))))
 
-(println (robots-to-string 11 7 (parse-robots example-robots)))
+;; (println (robots-to-string 11 7 (parse-robots example-robots)))
 
