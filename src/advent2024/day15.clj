eec35ac26e80608956e527274e83aaac18dd756d
Dave King <tildedave@gmail.com>
2024-12-15 06:32:15 -0500

    [2024] Day 15 - second star

--- src/advent2024/day15.clj
+++ src/advent2024/day15.clj
@@ -6,7 +6,8 @@ grid/out-of-bounds?
 
 (defn moved-squares [grid coords direction]
   ;; assumption: grid coords is @
-  (assert (= (grid/at grid coords) \@))
+  ;; (assert (= (grid/at grid coords) \@))
+  ;; part2 breaks this assumption; we'll use this function recursively
   (let [ray-direction (case direction
                         :up [0 -1]
                         :right [1 0]
@@ -21,6 +22,44 @@ grid/out-of-bounds?
          (case (grid/at grid next)
            \# []
            \O (recur next (conj affected [curr next]))
+           ;; part 2 logic can go right here, woo-hoo
+           ;; the logic is roughly the same
+           (\[ \])
+           (case direction
+             ;; going left or right, boring, just expand a bit more
+             (:left :right)
+             (let [next-next (mapv + next ray-direction)]
+               (recur next-next
+                      (-> affected
+                          (conj [curr next]
+                                [next next-next]))))
+             ;; here's the rub
+             ;; we view the box as an "actor" itself, pushing up.
+             ;; both sides of the box need to push.
+             ;; we have to also merge result, e.g. pushing a directly aligned
+             ;; box
+             (:up :down)
+             (let [[left-box right-box]
+                   (case (grid/at grid next)
+                     \] [(mapv + next [-1 0]) next]
+                     \[ [(mapv + next [1 0]) next])
+                   left-moved (moved-squares grid left-box direction)
+                   right-moved (moved-squares grid right-box direction)]
+               ;; there's an extra assumption on move order here
+               ;; we can probably use a sorter in the consumption to do it for
+               ;; us
+               ;; so a very cavalier approach here would be
+               (cond (empty? left-moved)
+                     []
+                     (empty? right-moved)
+                     []
+                     :else
+                     (vec (distinct
+                      (into (-> affected
+                               (conj [curr next])
+                               (conj [left-box (mapv + left-box ray-direction)])
+                               (conj [right-box (mapv + right-box ray-direction)]))
+                           (concat left-moved right-moved)))))))
            \. (conj affected [curr next])))))))
 
 (defn start-location [grid]
@@ -70,12 +109,20 @@ grid/out-of-bounds?
       (grid/assoc to (grid/at grid from))
       (grid/assoc from \.)))
 
+;; if moving left, want to process moving the leftmost (lower)
+(defn dependency-order [direction [_ t1] [_ t2]]
+  (case direction
+    :left (compare (first t1) (first t2))
+    :right (compare (first t2) (first t1))
+    :up (compare (second t1) (second t2))
+    :down (compare (second t2) (second t1))))
+
 (defn try-move [[grid curr] direction]
   (let [moved-squares (moved-squares grid curr direction)]
     (if (empty? moved-squares)
       [grid curr]
       ;; otherwise we modify the list
-      [(reduce (fn [grid [from to]] (swap-coords grid from to)) grid (rseq moved-squares))
+      [(reduce (fn [grid [from to]] (swap-coords grid from to)) grid (sort (partial dependency-order direction) moved-squares))
        (second (first moved-squares))])))
 
 (defn parse-direction [ch]
@@ -87,6 +134,8 @@ grid/out-of-bounds?
 
 (let [grid (grid/parse example-grid)]
   (try-move [grid (start-location grid)] :left))
+
+(def ^:dynamic part2? false)
 
 (def smaller-example
   '("########"
@@ -100,13 +149,26 @@ grid/out-of-bounds?
     ""
     "<^^>>>vv<v>>v<<"))
 
-
 (defn box-coords [grid]
-  (->> (grid/coords grid) (filter #(= (grid/at grid %) \O))))
+  (->> (grid/coords grid) (filter #(case (grid/at grid %) (\O \[) true false))))
+
+(defn expand-grid [grid]
+  (let [[max-x max-y] (grid/bounds grid)]
+    (vec
+     (for [y (range 0 max-y)]
+       (->>
+        (reduce into []
+                (for [x (range 0 max-x)]
+                  (case (grid/at grid [x y])
+                    \# [\# \#]
+                    \O [\[ \]]
+                    \. [\. \.]
+                    \@ [\@ \.]))))))))
 
 (defn execute-robot [lines]
   (let [[grid-lines direction-lines] (utils/split-by "" lines)
         grid (grid/parse grid-lines)
+        grid (if part2? (expand-grid grid) grid)
         [final-grid _] (reduce try-move
                            [grid (start-location grid)]
                            (->> direction-lines
@@ -115,6 +177,37 @@ grid/out-of-bounds?
          (map (fn [[x y]] (+ x (* 100 y))))
          (reduce +))))
 
+(execute-robot smaller-example)
+
 (execute-robot example)
 
 (execute-robot (utils/read-input "2024/day15.txt"))
+
+(expand-grid (grid/parse example-grid))
+
+(def double-push-example
+  '("#######"
+    "#...#.#"
+    "#.....#"
+    "#..OO@#"
+    "#..O..#"
+    "#.....#"
+    "#######"
+    ""
+    "<vv<<^^<<^^"))
+
+(defn execute-robot-debug [lines n]
+  (let [[grid-lines direction-lines] (utils/split-by "" lines)
+        grid (grid/parse grid-lines)
+        grid (if part2? (expand-grid grid) grid)
+        [final-grid _] (reduce try-move
+                               [grid (start-location grid)]
+                               (->> direction-lines
+                                    (mapcat (partial map parse-direction))
+                                    (take n)))]
+    final-grid))
+
+(binding [part2? true] (execute-robot-debug double-push-example 11))
+
+(binding [part2? true] (execute-robot example))
+(binding [part2? true] (utils/read-input "2024/day15.txt"))
