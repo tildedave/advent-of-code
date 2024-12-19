504bc6268f27631bd0b29f8e78221dd12331f35a
Dave King <tildedave@gmail.com>
2024-12-19 06:14:43 -0500

    [2024] Day 19 - second star; turns out memoization is good

--- src/advent2024/day19.clj
+++ src/advent2024/day19.clj
@@ -32,16 +32,36 @@
                    (filter (partial rec-f rec-f))
                    (empty?)
                    (not))))]
-    (partial f f)))
+    (partial f (memoize f))))
 
 (sort-by count > ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"])
 
-((can-make? ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]) "ubwu")
-
 (defn answer-part1 [lines]
   (let [[patterns towels] (parse-towels lines)
-        can-make (can-make? patterns)]
-    (count (filter can-make towels))))
+        can-make? (can-make? patterns)]
+    (count (filter can-make? towels))))
 
 (answer-part1 example)
 (answer-part1 (utils/read-input "2024/day19.txt"))
+
+(defn towel-count [patterns]
+  (let [patterns (sort-by count > patterns)
+        f (fn [rec-f str]
+            (if (empty? str)
+              1
+              (->> patterns
+                   (filter #(.startsWith str %))
+                   (map #(subs str (count %)))
+                   (map (partial rec-f rec-f))
+                   (reduce +))))]
+    (partial f (memoize f))))
+
+((towel-count ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]) "rrbgbr")
+
+(defn answer-part2 [lines]
+  (let [[patterns towels] (parse-towels lines)
+        towel-count (towel-count patterns)]
+    (reduce + (map towel-count towels))))
+
+(answer-part2 example)
+(answer-part2 (utils/read-input "2024/day19.txt"))
