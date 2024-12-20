651bf5a8ec2d4b8d9a15aaa921e8beccc14565ef
Dave King <tildedave@gmail.com>
2024-12-20 08:57:00 -0500

    [2024] Day 20 - almost there, off by one on lower path counts

--- src/advent2024/day20.clj
+++ src/advent2024/day20.clj
@@ -57,38 +57,13 @@
 ;; the walls
 ;; we can BFS or whatever from the inside-the-wall location
 
-(defn take-n-steps [distances start-coords end-coords n]
-  (loop [curr start-coords
-         total-steps 0]
-    (if (or (= curr end-coords) (= total-steps n))
-      curr
-      (let [d (distances curr)]
-        (recur
-         (->> grid/cardinal-directions
-              (map #(mapv + curr %))
-              (filter #(= (dec d) (distances %)))
-              (first))
-         (inc total-steps))))))
-
-(defn bfs-neighbor-visit [grid steps start end distances cheating-fuel]
-  (fn [[queue visited saves] next]
+(defn bfs-neighbor-visit [steps]
+  (fn [[queue visited] next]
     (cond
-      (contains? visited next) [queue visited saves]
-      (not= (grid/at grid next) \#)
-      [(conj queue {:coords next :steps (inc steps)})
-       (conj visited next)
-       (let [save (- (distances start)
-                     (distances next)
-                     (inc steps))]
-         ;; this coord actually needs to mark the end coord _as if_ we followed the closest path.  the save is still what we calculated.
-         (update
-          saves
-          (take-n-steps distances next end (- cheating-fuel (inc steps)))
-          (fnil #(conj % save) #{})))]
+      (contains? visited next) [queue visited]
       :else
       [(conj queue {:coords next :steps (inc steps)})
-       (conj visited next)
-       saves])))
+       (conj visited next)])))
 
 (defn search-part2 [grid cheating-fuel]
   (let [end (end-coords grid)
@@ -98,40 +73,70 @@
                                (fn [& _] false))
         path (reconstruct distances (start-coords grid) end)]
     (->> path
-        ;;  (take 1)
-         (mapcat
+         (map
           (fn [start]
-            (loop [queue (->> grid/cardinal-directions
-                              (filter (fn [d] (= (grid/at grid (mapv + d start)) \#)))
-                              (mapv (fn [d] {:coords (mapv + d start) :steps 1})))
+            (loop [queue [{:coords start :steps 0}]
                    visited #{}
                    saves {}]
               (if (empty? queue)
-                (->> saves (vals) (reduce set/union) (filter #(> % 0)))
-                (let [{:keys [coords steps]} (first queue)]
-                  (if (= steps cheating-fuel) ;; cheating fuel is gone
+                saves
+                (let [{:keys [coords steps]} (first queue)
+                      at-wall? (= \# (grid/at grid coords))
+                      saves (if at-wall?
+                              saves
+                              (assoc saves coords
+                                     (-
+                                      (distances start)
+                                      (distances coords)
+                                      steps)))]
+                  (if
+                   (or (= steps cheating-fuel) ;; out o'cheating fuel
+                       (= coords end))
                     (recur
                      (subvec queue 1)
                      visited
                      saves)
-                    (let [[queue visited saves]
+                    (let [[queue visited]
                           (reduce
-                           (bfs-neighbor-visit grid steps start end distances cheating-fuel)
-                           [(subvec queue 1) visited saves]
+                           (bfs-neighbor-visit steps)
+                           [(subvec queue 1) visited]
                            (grid/neighbors grid coords))]
                       (recur queue visited saves)))))))))))
 
+(defn count-paths [results]
+  (mapcat (fn [s] (->> (vals s) (filter #(> % 0)))) results))
+
 (=
- (frequencies (search-part2 (grid/parse-file "2024/day20-example.txt") 2))
+ (frequencies (count-paths (search-part2 (grid/parse-file "2024/day20-example.txt") 2)))
  (frequencies (search (grid/parse-file "2024/day20-example.txt"))))
+
+(count-paths (search-part2 (grid/parse-file "2024/day20-example.txt") 8))
+
+(let [f (frequencies (count-paths  (search-part2 (grid/parse-file "2024/day20-example.txt") 20)))]
+  ;; correct
+  (assert (= (f 76) 3))
+  (assert (= (f 74) 4))
+  (assert (= (f 72) 22))
+  (assert (= (f 70) 12))
+  (assert (= (f 68) 14))
+  (assert (= (f 66) 12))
+  (assert (= (f 64) 19))
+  ;; starts getting wrong
+  (assert (= (f 62) 20) (str "value was " (f 62) " needed " 20))
+  (assert (= (f 60) 23) (str "value was " (f 60) " needed " 23))
+  (assert (= (f 58) 25) (str "value was " (f 58) " needed " 25))
+  (assert (= (f 56) 39) (str "value was " (f 56) " needed " 39))
+  (assert (= (f 54) 29) (str "value was " (f 54) " needed " 29))
+  (assert (= (f 52) 31) (str "value was " (f 52) " needed " 31))
+  (assert (= (f 50) 32) (str "value was " (f 50) " needed " 32)))
+
+(defn answer-part2 [grid]
+  (let [saves (search-part2 grid 20)]
+    (count (filter #(>= % 100) saves))))
+
+(println "time to calculate")
+(println (answer-part2 (grid/parse-file "2024/day20.txt")))
 
 ;; OK great
 ;; quantity numbers are wonky
-
 ;; quantity numbers are still wonky
-(frequencies (search-part2 (grid/parse-file "2024/day20-example.txt") 20))
-
-
-(filter (fn [[_ v]] (contains? v 76)) (first (search-part2 (grid/parse-file "2024/day20-example.txt") 20)))
-
-(end-coords (grid/parse-file "2024/day20-example.txt"))
