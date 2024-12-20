e9ff6c6a89fd6f98fe0a48c32ce5fad8581adbbf
Dave King <tildedave@gmail.com>
2024-12-20 06:09:46 -0500

    [2024] Day 20 - checkpoint (almost there)

--- src/advent2024/day20.clj
+++ src/advent2024/day20.clj
@@ -2,7 +2,8 @@
   (:require
    [grid :as grid]
    [graph :as graph]
-   [utils :as utils]))
+   [utils :as utils]
+   [clojure.set :as set]))
 
 (defn char-position [grid ch]
   (->> (grid/coords grid) (filter #(= (grid/at grid %) ch)) (first)))
@@ -11,38 +12,6 @@
 (defn end-coords [grid] (char-position grid \E))
 
 (grid/parse-file "2024/day20-example.txt")
-
-;; dijsktra kind of sucks
-;; (defn search [grid]
-;;   (let [end-coords (end-coords grid)
-;;         distances (graph/dijkstra-search
-;;                    {:coords (start-coords grid) :cheat nil}
-;;                    (fn [{:keys [coords cheat] :as p}]
-;;                      (->> grid/cardinal-directions
-;;                           (map (fn [d]
-;;                                  (let [next (mapv + coords d)
-;;                                        is-wall? (= (grid/at grid next) \#)]
-;;                                    (cond
-;;                                      (and (nil? cheat) is-wall?)
-;;                                      {:coords (mapv + next d)
-;;                                       :cheat coords}
-;;                                      is-wall? nil
-;;                                      :else (assoc p :coords next)))))
-;;                           (remove nil?)))
-;;                    (fn [& _] false)
-;;                    (fn [current neighbor]
-;;                      (utils/manhattan-distance
-;;                       (:coords current)
-;;                       (:coords neighbor))))
-;;         no-cheat-distance (distances {:coords  end-coords :cheat nil})]
-;;     [(->> distances
-;;          (filter (fn [[k _]] (= end-coords (:coords k))))
-
-;;          )]))
-
-;; OK we'll try something that might be invalidated by p2
-;; I guess we might "cheat" into a square that we haven't gotten to
-;; so a*-search
 
 (defn reconstruct [distances start-coords]
   (loop [curr start-coords
@@ -83,3 +52,62 @@
     (count (filter #(>= % 100) saves))))
 
 (answer-part1 (grid/parse-file "2024/day20.txt"))
+
+;; part 2 is basically the same instead we can do a bit more wandering through
+;; the walls
+;; we can BFS or whatever from the inside-the-wall location
+
+(defn search-part2 [grid cheating-fuel]
+  (let [end-coords (end-coords grid)
+        distances
+        (graph/dijkstra-search end-coords
+                               (fn [c] (grid/neighbors grid c grid/cardinal-directions #(= % \#)))
+                               (fn [& _] false))
+        path (reconstruct distances (start-coords grid))]
+    (->> path
+         (mapcat
+          (fn [c]
+            (->> grid/cardinal-directions
+                 (filter (fn [d] (= (grid/at grid (mapv + d c)) \#)))
+              ;; OK we're in the wall, we can walk inside the wall for up to
+              ;; 19 more steps
+              ;; I'll manually code this since it is fiddly
+                 ;; not clear if this is going to explode; BFS 20 steps should
+                 ;; be tractable
+                 (mapcat
+                  (fn [d]
+                    (loop [queue [{:coords (mapv + d c) :steps 1}]
+                           seen #{}
+                           saves {}]
+                      (if (empty? queue)
+                        (->> (vals saves) (filter #(> % 0)))
+                        (let [{:keys [coords steps]} (first queue)]
+                          (if (= steps cheating-fuel) ;; cheating fuel is gone
+                            (recur (rest queue) (conj seen coords) saves)
+                            (let [[queue seen saves]
+                              (reduce
+                               (fn [[queue seen saves] next]
+                                 (cond
+                                   (contains? seen next) [queue seen saves]
+                                   (not= (grid/at grid next) \#)
+                                   [queue seen
+                                    (assoc saves next
+                                           (- (distances c)
+                                              (distances next)
+                                              (inc steps)))]
+                                   :else
+                                   [(conj queue {:coords next :steps (inc steps)})
+                                    seen
+                                    saves]))
+                                [(rest queue) (conj seen coords) saves]
+                                (grid/neighbors grid coords))]
+                              (recur queue seen saves))))))))))))))
+
+(=
+ (frequencies (search-part2 (grid/parse-file "2024/day20-example.txt") 2))
+ (frequencies (search (grid/parse-file "2024/day20-example.txt"))))
+
+;; OK great
+;; quantity numbers are wonky - claims 22 cheats that save 72, I only see 8
+;; this is probably because I'm combining directions
+(frequencies (filter #(>= % 50) (search-part2 (grid/parse-file "2024/day20-example.txt") 20)))
