cb26885d31972e3b144e6b1f6fdc822395ab57de
Dave King <tildedave@gmail.com>
2025-12-01 13:32:21 -0500

    [2022] Day 9 - second star

--- haskell/src/2022/day9.hs
+++ haskell/src/2022/day9.hs
@@ -23,10 +23,28 @@ isAdjacent (hx, hy) (tx, ty) =
     (1, 1) -> True
     _ -> False
 
-move :: Direction -> (Coord2d, Coord2d) -> (Coord2d, Coord2d)
-move d (h, t) =
+compareInt :: (Ord a) => a -> a -> Int
+compareInt x y = case compare x y of
+  LT -> -1
+  GT -> 1
+  EQ -> 0
+
+tailStep :: Coord2d -> Coord2d -> Coord2d
+tailStep (hx, hy) (tx, ty) =
+  let (dtx, dty) = (compareInt hx tx, compareInt hy ty)
+   in (tx + dtx, ty + dty)
+
+follow :: Coord2d -> [Coord2d] -> [Coord2d]
+follow _ [] = []
+follow next (x : xs) =
+  let xnext = (if isAdjacent next x then x else tailStep next x) in xnext : follow xnext xs
+
+move :: [Coord2d] -> Direction -> [Coord2d]
+move (h : tails) d =
+  -- for each item in tails list, if it's no longer adjacent to the p, move to the previous spot
   let nextH = add2 (delta2d d) h
-   in if isAdjacent t nextH then (nextH, t) else (nextH, h)
+   in nextH : follow nextH tails
+move _ _ = error "invalid"
 
 expand :: (Direction, Int) -> [Direction]
 expand (d, n) = map (const d) [1 .. n]
@@ -34,9 +52,13 @@ expand (d, n) = map (const d) [1 .. n]
 part1 :: T.Text -> Int
 part1 s =
   S.size $
-    foldr (S.insert . snd) S.empty $
-      scanl (flip move) ((0, 0), (0, 0)) $
+    foldr (S.insert . last) S.empty $
+      scanl move (replicate 2 (0, 0)) $
         concatMap (expand . parseLine) (T.splitOn "\n" s)
 
 part2 :: T.Text -> Int
-part2 _ = 1
+part2 s =
+  S.size $
+    foldr (S.insert . last) S.empty $
+      scanl move (replicate 10 (0, 0)) $
+        concatMap (expand . parseLine) (T.splitOn "\n" s)
