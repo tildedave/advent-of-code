618d80193d289bde546b489efed6418d7e027cd6
Dave King <dking@atlassian.com>
2025-12-08 14:15:32 -0500

    [2022] Day 14 - part2 works for examples, not finishing in time for input

--- haskell/src/2022/day14.hs
+++ haskell/src/2022/day14.hs
@@ -6,6 +6,7 @@ import Data.Foldable qualified as S
 import Data.List (unfoldr)
 import Data.Set qualified as S
 import Data.Text qualified as T
+import Debug.Trace (traceShow)
 import Util (Coord2d, add2, sub2)
 
 -- | parseCoord
@@ -53,6 +54,12 @@ sandFall cutoff (x, y) blocked =
     downLeft = (x - 1, y + 1)
     downRight = (x + 1, y + 1)
 
+-- | Examples
+-- >>> s = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9"
+-- >>> part1 s
+-- 24
+-- >>> part2 s
+-- 93
 part1 :: T.Text -> Int
 part1 t =
   length $
@@ -66,5 +73,29 @@ part1 t =
     walls = foldr (S.union . parseLine) S.empty (T.splitOn "\n" t)
     cutoff = abyssY walls
 
+sandFallPart2 :: Int -> Coord2d -> S.Set Coord2d -> Coord2d
+sandFallPart2 cutoff (x, y) blocked =
+  if y == cutoff
+    then
+      (x, y)
+    else
+      let next = filter (\c -> not $ c `S.elem` blocked) [down, downLeft, downRight]
+       in (if null next then (x, y) else sandFallPart2 cutoff (head next) blocked)
+  where
+    down = (x, y + 1)
+    downLeft = (x - 1, y + 1)
+    downRight = (x + 1, y + 1)
+
 part2 :: T.Text -> Int
-part2 _ = 1
+part2 t =
+  (+ 1) $
+    length $
+      unfoldr
+        ( \walls' -> case sandFallPart2 cutoff (500, 0) walls' of
+            (500, 0) -> Nothing
+            next -> Just (traceShow next next, S.insert next walls')
+        )
+        walls
+  where
+    walls = foldr (S.union . parseLine) S.empty (T.splitOn "\n" t)
+    cutoff = abyssY walls + 1
